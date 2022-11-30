30d6ef2bdfc804f353af24913ac08c6da408144c
Valentina Ruiz <v.zruiz06@gmail.com>
2022-11-30 14:14:55 -0800

    starting work on wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -8,6 +8,17 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  // if word.length <= 7
+  if (word.length<= 7) {
+    // add 8pts to counter
+    score === 8;
+  } else {
+    // else counter = 0
+    score === 0;
+  }
+  // loop through letter in word
+  // if letter in obj add point value to counter
+  // return counter
 };
 
 export const highestScoreFrom = (words) => {
