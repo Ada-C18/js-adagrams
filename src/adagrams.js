16921adb46a5dcec5f36c75b321b8927322c47ba
Selam Chaka <selblessed@gmail.com>
2022-11-30 17:26:12 -0500

    Add Esline

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,54 @@
+const LETTERPOOL = {
+  'A': 9, 
+  'B': 2, 
+  'C': 2, 
+  'D': 4, 
+  'E': 12, 
+  'F': 2, 
+  'G': 3, 
+  'H': 2, 
+  'I': 9, 
+  'J': 1, 
+  'K': 1, 
+  'L': 4, 
+  'M': 2, 
+  'N': 6, 
+  'O': 8, 
+  'P': 2, 
+  'Q': 1, 
+  'R': 6, 
+  'S': 4, 
+  'T': 6, 
+  'U': 4, 
+  'V': 2, 
+  'W': 2, 
+  'X': 1, 
+  'Y': 2, 
+  'Z': 1
+}
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  function drawLetters() {
+    let letterFreq = {};
+    let randomLetters=[];
+    while ((randomLetters.length) < 26) {
+      let r = Object.keys(LETTERPOOL)[Math.floor(Math.random()*Object.keys(LETTERPOOL).length)];
+      randomLetters.push(r);
+    }
+    for (let letter of randomLetters) {
+        if (letter in letterFreq) {
+            letterFreq.letter += 1;
+        } else {
+            letterFreq.letter = 1;
+        }
+        }
+    return (randomLetters.slice(0,10))
+}
+
+console.log(drawLetters())
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
