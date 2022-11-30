25d30ef0b69b412a95ac170a4e3099321db5ca30
Makeesha Pruitt <makeeshalp@gmail.com>
2022-11-30 14:12:51 -0500

    started wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,46 @@
 export const drawLetters = () => {
+  const letterPool = {
+    "A": 9,
+    "B": 2,
+    "C": 2,
+    "D": 4,
+    "E": 12,
+    "F": 2,
+    "G": 3,
+    "H": 2,
+    "I": 9,
+    "J": 1,
+    "K": 1,
+    "L": 4,
+    "M": 2,
+    "N": 6,
+    "O": 8,
+    "P": 2,
+    "Q": 1,
+    "R": 6,
+    "S": 4,
+    "T": 6,
+    "U": 4,
+    "V": 2,
+    "W": 2,
+    "X": 1,
+    "Y": 2,
+    "Z": 1,
+};
+  const letterList = [];
+  const poolList = [];
+
+  for(let i = 0; i < letterPool.length; ++i)
   // Implement this method for wave 1
 };
+// i = 0
+//     for letter in LETTER_POOL.keys():
+//         for i in range(LETTER_POOL[letter]):
+//             pool_list.append(letter)
+//     letter_list = random.sample(pool_list, 10)
+//     return letter_list
+
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
