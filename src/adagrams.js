8ec8cd168970a59a6323b016b39d9b79c3521d4f
Atefeh Mirzaei <atifaayman@gmail.com>
2022-11-30 09:15:25 -0800

    completing the function scoreWord andthe missing test then  passing the 4 tests in Wave3

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,31 +1,31 @@
 
 const poolOfLetters = {
-    A : 9,
-    B : 2,
-    C : 2, 
-    D : 4,
-    E : 12,
-    F : 2,
-    G : 3,
-    H : 2,
-    I : 9,
-    J : 1,
-    K : 1,
-    L : 4,
-    M : 2,
-    N : 6,
-    O : 8,
-    P : 2,
-    Q : 1,
-    R : 6,
-    S : 4,
-    T : 6,
-    U : 4,
-    V : 2,
-    W : 2,
-    X : 1,
-    Y : 2,
-    Z :1
+  A : 9,
+  B : 2,
+  C : 2, 
+  D : 4,
+  E : 12,
+  F : 2,
+  G : 3,
+  H : 2,
+  I : 9,
+  J : 1,
+  K : 1,
+  L : 4,
+  M : 2,
+  N : 6,
+  O : 8,
+  P : 2,
+  Q : 1,
+  R : 6,
+  S : 4,
+  T : 6,
+  U : 4,
+  V : 2,
+  W : 2,
+  X : 1,
+  Y : 2,
+  Z :1
 }
 
 export const drawLetters = () => {
@@ -72,7 +72,51 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+
+  /*
+  Input: word that is a string of letters
+  Output: The score of the word
+  */
+  
+  const scoreChart = {
+    A: 1, 
+    B: 3, 
+    C: 3, 
+    D: 2, 
+    E: 1, 
+    F: 4, 
+    G: 2, 
+    H: 4, 
+    I: 1, 
+    J: 8, 
+    K: 5, 
+    L: 1, 
+    M: 3, 
+    N: 1, 
+    O: 1, 
+    P: 3, 
+    Q: 10, 
+    R: 1, 
+    S: 1, 
+    T: 1, 
+    U: 1, 
+    V: 4, 
+    W: 4, 
+    X: 8, 
+    Y: 4, 
+    Z: 10
+  };
+
+  let score = 0;
+  for ( const letter of  word.toUpperCase()) {
+    score += scoreChart[letter];
+  };
+
+  if ( word.length > 6) {
+    score += 8 ;
+  };
+  
+  return score ;
 };
 
 export const highestScoreFrom = (words) => {
