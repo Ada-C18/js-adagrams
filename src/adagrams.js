d019de8266466d0382039a20c4449c3acc898696
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-11-30 12:15:04 -0800

    Wave 1 draft 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,14 +1,16 @@
 export const drawLetters = () => {
   let alphabetPool = 'aaaaaaaaaabbccddddeeeeeeeeeeeeffggghhiiiiiiiiijkllllmmnnnnnnooooooooppqrrrrrrssssttttttuuuuvvwwxyyz'
+  alphabetPool = alphabetPool.toUpperCase()
   let i = 0;
   let playerHand = []
   
   do {
     i++;
-    let singleLetter = alphabetPool.charAt(Math.random()*alphabetPool.length);
+    let singleLetter = alphabetPool.charAt(Math.random() * alphabetPool.length);
     playerHand.push(singleLetter);
     alphabetPool = alphabetPool.replace(singleLetter, '');
   } while (i < 10);
+}
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
