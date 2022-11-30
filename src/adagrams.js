f7ee7028a22b42e20f742c9303813a1c59f2b31a
yangsu <suyang.sophia@outlook.com>
2022-11-30 15:29:52 -0800

    wave 4 finished just throw test don't pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,6 +26,34 @@ const LETTER_POOL_COUNT = {
   Y: 2,
   Z: 1,
 };
+const SCORE_CHART = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
 // --------------------------------------------------------wave 1------------------------------------------------
 export const drawLetters = () => {
   // Implement this method for wave 1
@@ -74,11 +102,54 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
   return true;
 };
-
+// --------------------------------------------------------wave 3------------------------------------------------
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let upper_case_word = word.toUpperCase();
+  let score_totla = 0;
+  if (upper_case_word.length === 0) {
+    return score_totla;
+  }
+  if (upper_case_word.length >= 7) {
+    score_totla += 8;
+  }
+  for (let letter of upper_case_word) {
+    if (letter in SCORE_CHART) {
+      score_totla += SCORE_CHART[letter];
+    }
+  }
+  return score_totla;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+  let best_word = [];
+  let bestWordObj = {};
+  for (let i = 0; i < words.length; i++) {
+    let score = scoreWord(words[i]);
+    if (best_word.length === 0) {
+      best_word.push(words[i]);
+      best_word.push(score);
+    } else if (score > best_word[1]) {
+      best_word[0] = words[i];
+      best_word[1] = score;
+    } else if (score === best_word[1]) {
+      if (
+        best_word[0].length === words[i].length ||
+        best_word[0].length === 10
+      ) {
+        continue;
+      } else if (
+        best_word[0].length > words[i].length ||
+        words[i].length === 10
+      ) {
+        best_word[0] = words[i];
+      } else if (best_word[0].length < words[i].length) {
+        continue;
+      }
+    }
+  }
+  bestWordObj["score"] = best_word[1];
+  bestWordObj["word"] = best_word[0];
+  return bestWordObj;
 };
