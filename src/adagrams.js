f993aab1240acd3a544f94f487550f014b042c4b
Nina <nina.sohn@gmail.com>
2022-11-29 16:13:38 -0800

    Wave 1 complete. Added two helper functions called randomInt and removeLetter and completed drawLetters function.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,69 @@
+//helper functions
+const randomInt = function (max) {
+  return Math.floor(Math.random() * max);
+};
+
+const removeLetter = function (arr, target) {
+  var idx = arr.indexOf(target);
+  if (idx > -1) {
+    arr.splice(idx, 1);
+  }
+  return arr;
+};
+
+//main functions
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const bagOfLetters = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+  const arrayOfLetters = [];
+  Object.keys(bagOfLetters).forEach((letter) => {
+    for (let i = 0; i < bagOfLetters[letter]; i++) {
+      arrayOfLetters.push(letter);
+    }
+  });
+  const hand = [];
+  while (hand.length !== 10) {
+    let letterIndex = randomInt(arrayOfLetters.length);
+    hand.push(arrayOfLetters[letterIndex]);
+    removeLetter(arrayOfLetters, arrayOfLetters[letterIndex]);
+  }
+  return hand;
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+// };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
+// };
