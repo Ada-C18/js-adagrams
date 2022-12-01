56d9448160dde836a9e4eacc5afff01efeb938dd
Rose Le <rosehle@protonmail.com>
2022-11-30 20:02:53 -0800

    Completed Wave 01

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,54 @@
+export const letterPool = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+export const createLetterPoolArr = (letterPool) => {
+  let letterPoolArr = [];
+  for (const letter in letterPool) {
+    for (let i = 0; i < letterPool[letter]; i++) {
+      letterPoolArr.push(letter);
+    }
+  }
+  return letterPoolArr;
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const letterPoolArr = createLetterPoolArr(letterPool);
+  let hand = [];
+  while (hand.length < 10) {
+    let randomLetter =
+      letterPoolArr[Math.floor(Math.random() * letterPoolArr.length)];
+    let count = hand.filter((x) => x === randomLetter).length;
+    if (count < letterPool[randomLetter]) {
+      hand.push(randomLetter);
+    }
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
