1a42df2f4c856c2735502c401c6ccdb7f0a7c1e6
lisafeng <lisafenglisa@yahoo.com>
2022-12-01 13:10:25 -0500

    finished wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,41 @@
+const LETTERPOOL = [
+  'A','A','A','A','A','A','A','A','A',
+  'B','B',
+  'C','C',
+  'D','D','D','D',
+  'E','E','E','E','E','E','E','E','E','E','E','E',
+  'F','F',
+  'G','G','G',
+  'H','H',
+  'I','I','I','I','I','I','I','I','I',
+  'J',
+  'K',
+  'L','L','L','L',
+  'M','M',
+  'N','N','N','N','N','N',
+  'O','O','O','O','O','O','O','O',
+  'P','P',
+  'Q',
+  'R','R','R','R','R','R',
+  'S','S','S','S',
+  'T','T','T','T','T','T',
+  'U','U','U','U',
+  'V','V',
+  'W','W',
+  'X',
+  'Y','Y',
+  'Z'
+]
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let letters = [];
+  let letterPoolCopy = [...LETTERPOOL];
+  while (letters.length < 10) {
+    const random = Math.floor(Math.random() * letterPoolCopy.length);
+    letters.push(letterPoolCopy[random]);
+    letterPoolCopy.splice(random, 1);
+  }
+  return letters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
