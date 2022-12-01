75e479a1a8df94f3068597fc8370d84ef038e150
Samantha Hunt <ariaandsam@gmail.com>
2022-12-01 15:40:39 -0800

    finished waves 1-3, forgot to commit

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,13 +1,110 @@
+const letterQuantity = {
+  "A": 9,
+  "B": 2,
+  "C": 2,
+  "D": 4,
+  "E": 12,
+  "F": 2,
+  "G": 3,
+  "H": 2,
+  "I": 9,
+  "J": 1,
+  "K": 1,
+  "L": 4,
+  "M": 2,
+  "N": 6,
+  "O": 8,
+  "P": 2,
+  "Q": 1,
+  "R": 6,
+  "S": 4,
+  "T": 6,
+  "U": 4,
+  "V": 2,
+  "W": 2,
+  "X": 1,
+  "Y": 2,
+  "Z": 1
+}
+const letterValue = {
+  "A": 1,
+  "B": 3,
+  "C": 3,
+  "D": 2,
+  "E": 1,
+  "F": 4,
+  "G": 2,
+  "H": 4,
+  "I": 1,
+  "J": 8,
+  "K": 5,
+  "L": 1,
+  "M": 3,
+  "N": 1,
+  "O": 1,
+  "P": 3,
+  "Q": 10,
+  "R": 1,
+  "S": 1,
+  "T": 1,
+  "U": 1,
+  "V": 4,
+  "W": 4,
+  "X": 8,
+  "Y": 4,
+  "Z": 10
+}
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letters_drawn = {};
+  const letters = []
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i=0; i < 10; i++){
+    let letter = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
+    if (!letters_drawn[letter]){
+      letters_drawn[letter] = 1
+      letters.push(letter)
+    } else if (letters_drawn[letter] < letterQuantity[letter]){
+      letters_drawn[letter] += 1
+      letters.push(letter)
+    }
+  }
+  return letters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  const letterBankCount = {}
+  for (const letter of lettersInHand){
+    if (!letterBankCount[letter]){
+      letterBankCount[letter] = 1
+    } else {
+      letterBankCount[letter] += 1
+    }
+  }
+  for (const letter of input){
+    if (letter in letterBankCount && letterBankCount[letter] > 0){
+      letterBankCount[letter] -= 1
+      continue
+    } else{
+      return false;
+    }
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let upperWord = word.toUpperCase()
+  let score = 0
+  if (upperWord.length >= 7){
+    score += 8
+  }
+  for (const letter of upperWord) {
+    score += letterValue[letter]
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
