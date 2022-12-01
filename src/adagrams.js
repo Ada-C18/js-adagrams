e801e51aaa1860876fade28a31f6db118175c428
Rose Le <rosehle@protonmail.com>
2022-11-30 20:29:39 -0800

    Completed Wave 03

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ export const letterPool = {
   Z: 1,
 };
 
+export const scoreChart = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
+
 export const createLetterPoolArr = (letterPool) => {
   let letterPoolArr = [];
   for (const letter in letterPool) {
@@ -64,7 +93,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const fixedWord = word.toUpperCase();
+  let score = 0;
+  for (let letter of fixedWord) {
+    score += scoreChart[letter];
+  }
+  if (fixedWord.length >= 7) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
