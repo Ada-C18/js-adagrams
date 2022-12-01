8dfce158d3db7cd26f99e9b8a09d58a450456c69
Puja Mukherjee <pujam836@gmail.com>
2022-11-30 22:54:26 -0800

    wrote function to calculate score of a given string wave 3 and ran testcase 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -69,9 +69,49 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
+export const scoreWord = (word) => {
+  // Implement this method for wave 3
+  let scoreDict = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+
+  let score = 0;
+  word = word.toUpperCase();
+  for (var i = 0; i < word.length; i++) {
+    if (word.charAt(i) in scoreDict) {
+      score = score + scoreDict[word.charAt(i)];
+    }
+  }
+  if (word.length > 6) {
+    score += 8;
+  }
+  return score;
+};
 
 // export const highestScoreFrom = (words) => {
 //   // Implement this method for wave 4
