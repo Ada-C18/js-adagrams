f21747f1c59c367652853fa01b6d8cca18f2d6f4
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2022-12-01 13:47:06 -0800

    wave 1 complete

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,68 @@
+const LETTER_POOL = {
+  A: 9,
+  N: 6,
+  B: 2,
+  O: 8,
+  C: 2,
+  P: 2,
+  D: 4,
+  Q: 1,
+  E: 12,
+  R: 6,
+  F: 2,
+  S: 4,
+  G: 3,
+  T: 6,
+  H: 2,
+  U: 4,
+  I: 9,
+  V: 2,
+  J: 1,
+  W: 2,
+  K: 1,
+  X: 1,
+  L: 4,
+  Y: 2,
+  M: 2,
+  Z: 1,
+};
+
+const LETTER_POINTS = {
+  A: 1,
+  N: 1,
+  B: 3,
+  O: 1,
+  C: 3,
+  P: 3,
+  D: 2,
+  Q: 10,
+  E: 1,
+  R: 1,
+  F: 4,
+  S: 1,
+  G: 2,
+  T: 1,
+  H: 4,
+  U: 1,
+  I: 1,
+  V: 4,
+  J: 8,
+  W: 4,
+  K: 5,
+  X: 8,
+  L: 1,
+  Y: 4,
+  M: 3,
+  Z: 10,
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let pool = Object.entries(LETTER_POOL).reduce(
+    (acc, [letter, count]) => acc.concat(Array(count).fill(letter)),
+    []
+  );
+  let shuffled = pool.sort((a, b) => 0.5 - Math.random());
+  return Array.from(Array(10), () => shuffled.pop());
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
