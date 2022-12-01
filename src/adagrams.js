cf05ccba97ad394b751abf19d1b44ab98d42911c
Selam Chaka <selblessed@gmail.com>
2022-12-01 18:38:37 -0500

    passing tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,38 +27,90 @@ const LETTERPOOL = {
   'Z': 1
 }
 
-
 export const drawLetters = () => {
-  // Implement this method for wave 1
-  function drawLetters() {
     let letterFreq = {};
-    let randomLetters=[];
-    while ((randomLetters.length) < 26) {
-      let r = Object.keys(LETTERPOOL)[Math.floor(Math.random()*Object.keys(LETTERPOOL).length)];
-      randomLetters.push(r);
+    let selectedLetters = [];
+    while ((selectedLetters.length) < 10) {
+      let random = Object.keys(LETTERPOOL)[Math.floor(Math.random()*Object.keys(LETTERPOOL).length)];
+          if (random in letterFreq) {
+              letterFreq[random] += 1;
+          } else {
+              letterFreq[random] = 1;
+          if (letterFreq[random] <= LETTERPOOL[random]){
+            selectedLetters.push(random)
+          }        
+          }        
     }
-    for (let letter of randomLetters) {
-        if (letter in letterFreq) {
-            letterFreq.letter += 1;
-        } else {
-            letterFreq.letter = 1;
-        }
-        }
-    return (randomLetters.slice(0,10))
-}
-
-console.log(drawLetters())
-
+    return (selectedLetters)
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let lettersInHandCopy = lettersInHand
+  for (let letter of input.toUpperCase()) {
+    if (!lettersInHandCopy.includes(letter))  {
+      return false;
+    } 
+    lettersInHandCopy.splice(0, 1, letter);
+    }
+    return true;
+};
+
+
+let SCORECHART = { 
+  'A': 1, 
+  'B': 3, 
+  'C': 3, 
+  'D': 2, 
+  'E': 1, 
+  'F': 4, 
+  'G': 2, 
+  'H': 4, 
+  'I': 1, 
+  'J': 8, 
+  'K': 5, 
+  'L': 1, 
+  'M': 3, 
+  'N': 1, 
+  'O': 1, 
+  'P': 3, 
+  'Q': 10, 
+  'R': 1, 
+  'S': 1, 
+  'T': 1, 
+  'U': 1, 
+  'V': 4, 
+  'W': 4, 
+  'X': 8, 
+  'Y': 4, 
+  'Z': 10
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let score = 0
+  for(let letter of word.toUpperCase()){
+    score += SCORECHART[letter];
+  }
+  if (word.length > 6){
+    score +=8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let highestScore = {'score':0,'word':''};
+
+  for (let word of words){
+    let wordScore = scoreWord(word);
+    if (wordScore > highestScore['score']) {
+        highestScore['score'] = wordScore;
+        highestScore['word']= word;
+    } else if (wordScore === highestScore['score']){
+        if ( (word.length < highestScore['word'].length) && (highestScore['word'].length !== 10)){
+            highestScore['word']= word;
+        } else if ((word.length ===10) && (highestScore['word'].length !== 10)) {
+            highestScore['word']= word;
+        }
+  }
+  }
+  return highestScore;
 };
