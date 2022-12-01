b30aa9a99d34525319765a15ec5a01353907efd5
June Valentino <vintydesign@gmail.com>
2022-11-30 21:55:55 -0800

    added helper function to generate a list of unique letters for the player to draw from

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,49 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+};
+
+const generate_letter_list = () => {
+  /* 
+  Helper Function - generates an array that contains an instance of every letter in 
+  the LETTER_POOL object, incl duplicates
+  */
+  const letter_arr = [];
+
+  for (const letter in LETTER_POOL) {
+    for (let i = 0; i <= LETTER_POOL[letter]; i++) {
+      letter_arr.push(letter);
+    }
+  }
+  return letter_arr;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
