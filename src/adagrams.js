da7e508ebe33316ba494017419adcc87e703d216
Jan li <bingox29@gmail.com>
2022-12-01 13:17:46 -0500

    finished wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,52 @@
+const letterPOOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
 export const drawLetters = () => {
   // Implement this method for wave 1
+
+  let pool_list = [];
+  for (const letter in letterPOOL) {
+    for (let i = 0; i < letterPOOL[letter]; i++) {
+      pool_list.push(letter);
+    }
+  }
+  const shuffled = pool_list.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, 10);
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+// };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
+// };
