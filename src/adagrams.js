34cf9799df617cb93654a37e7f2f4001c30c9eff
Steffanie Lopez <lopezsteffanie8@gmail.com>
2022-12-01 11:12:41 -0800

    working on wave1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,87 @@
-export const drawLetters = () => {
-  // Implement this method for wave 1
-};
-console.log("Hi")
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+function range(start, stop, step) {
+  if (typeof stop == 'undefined') {
+      // one param defined
+      stop = start;
+      start = 0;
+  }
+
+  if (typeof step == 'undefined') {
+      step = 1;
+  }
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+      return [];
+  }
+
+  var result = [];
+  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+      result.push(i);
+  }
+
+  return result;
 };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
+const drawLetters = () => {
+  const letterPool = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+  };
+
+  let returnList = [];
+  let letterBank = [];
+
+  for (let [key,value] of Object.entries(letterPool)) {
+    for (value in range(value)) {
+      letterBank.push(key);
+    }
+  }
+
+  while (returnList.length < 10) {
+    let letter = letterBank[Math.floor(Math.random()* letterBank.length)];
+    returnList.push(letter);
+    let index = array.indexOf(letter);
+    letterBank.splice(index, 1);
+
+  console.log(returnList);
+  console.log(letterBank);
+  return returnList;
 };
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+drawLetters()
+
+
+// export const usesAvailableLetters = (input, lettersInHand) => {
+//   // Implement this method for wave 2
+// };
+
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
+
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
 };
