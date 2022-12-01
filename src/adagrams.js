a4f5dae14afdceac27909c17c9b3a51c7f16a7e6
Erika Torres Figueroa <torrespurple@gmail.com>
2022-11-30 23:03:50 -0500

    completed wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,55 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let drawPool = [];
+  const LETTER_POOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+  for (let [key, value] of Object.entries(LETTER_POOL)) {
+    for (let i = 0; i < value; i++) {
+      drawPool.push(key);
+    }
+  }
+  const hand = []; 
+  for (let i = 0; i < 10; i++) {
+    let pieceIndex = Math.floor(Math.random() * drawPool.length);
+    let piece = drawPool[pieceIndex];
+    hand.push(piece);
+    const index = drawPool.indexOf(piece);
+    if (index > -1) {
+      drawPool.splice(index, 1);
+    }
+  }
+  return hand;
 };
 
+
+// const rand = Math.floor(Math.random()*100);
+  // console.log(rand);
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
 };
