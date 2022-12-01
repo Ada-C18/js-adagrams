88fb645665e969716cc124d3876e575bffec73a7
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2022-12-01 12:35:16 -0800

    wave 2 complete- draft 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -15,14 +15,32 @@ export const drawLetters = () => {
 }
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // check whether type(input) === str
-  
-  // return true if: 
-  // - used letter is available in hand
-  
-  // return false if: 
+   // check whether type(input) === str
+  // create a dictionary that will house the lettersInHand (key) and number of occurences (value)
+  let playerHand = {}
+  for (let i of lettersInHand) {
+    if (i in playerHand) {
+      playerHand[i] += 1
+    } else {
+      playerHand[i] = 1
+    }
+  } 
+  // return false if:
   // - used letter in not available in hand
-  // - letter has been used in input word once
+  for (let j of input) {
+    if (!(j in playerHand)) {
+      return false;
+    } else {
+      playerHand[j] -= 1
+    }
+  }
+  // return true if:
+  // - used letter is available in hand
+  // return false if:
+  // - letter has been used in input word more times than is availible in hand
+  return Object.values(playerHand).every(
+    value => value >= 0
+  )
 };
 
 export const scoreWord = (word) => {
