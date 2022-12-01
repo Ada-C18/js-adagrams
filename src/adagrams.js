b6f07abc83e12ef2196949d320afa8b943ae3538
Billie Betts <lily.a.betts@gmail.com>
2022-12-01 13:09:31 -0500

    added letter data constant

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,3 +1,32 @@
+LETTER_DATA = [
+  { letter: "A", quantity: 9, value: 1 },
+  { letter: "B", quantity: 2, value: 3 },
+  { letter: "C", quantity: 2, value: 3 },
+  { letter: "D", quantity: 4, value: 2 },
+  { letter: "E", quantity: 12, value: 1 },
+  { letter: "F", quantity: 2, value: 4 },
+  { letter: "G", quantity: 3, value: 2 },
+  { letter: "H", quantity: 2, value: 4 },
+  { letter: "I", quantity: 9, value: 1 },
+  { letter: "J", quantity: 1, value: 8 },
+  { letter: "K", quantity: 1, value: 5 },
+  { letter: "L", quantity: 4, value: 1 },
+  { letter: "M", quantity: 2, value: 3 },
+  { letter: "N", quantity: 6, value: 1 },
+  { letter: "O", quantity: 8, value: 1 },
+  { letter: "P", quantity: 2, value: 3 },
+  { letter: "Q", quantity: 1, value: 10 },
+  { letter: "R", quantity: 6, value: 1 },
+  { letter: "S", quantity: 4, value: 1 },
+  { letter: "T", quantity: 6, value: 1 },
+  { letter: "U", quantity: 4, value: 1 },
+  { letter: "V", quantity: 2, value: 4 },
+  { letter: "W", quantity: 2, value: 4 },
+  { letter: "X", quantity: 1, value: 8 },
+  { letter: "Y", quantity: 2, value: 4 },
+  { letter: "Z", quantity: 1, value: 10 },
+];
+
 export const drawLetters = () => {
   // Implement this method for wave 1
 };
