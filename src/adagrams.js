cc89fc12262bd6f4441ff51a6c00d2b77df18906
Rahel Kemal <rahelskemal@gmail.com>
2022-12-01 08:18:11 -0800

    added functions to get score of word

--- src/adagrams.js
+++ src/adagrams.js
@@ -10,10 +10,6 @@ export const drawLetters = () => {
   let handList = []
   let tempHandobj = Object.assign({}, letterBank);
 
-    // while hand list is less than 10 append a random letter to the list and if it 
-  // is not 0 in the copy dictionary append and -1 from that letter
-  // return the hand_list
-  // 
   while (handList.length < 10) {
     let randNum = Math.floor(Math.random() * 26);
     let pickedLetter = Object.keys(tempHandobj)[randNum];
@@ -29,21 +25,75 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   let word = input.toUpperCase()
+  let validInput = true;            
   
-
   for (const letter of word) {
-    // let letterCount = lettersInHand.match(letter)
     if (!lettersInHand.includes(letter)) {
-      return false
+      validInput = false;
+    }
+    lettersInHand.splice(0, 1, letter)
+  }
+  return validInput;
+};
+
+  // Implement this method for wave 3
+export const scoreWord = (word) => {
+
+  let upperWord = word.toUpperCase()
+  let score = 0;
+  
+  let list1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
+  let list2 = ['D', 'G'];
+  let list3 = ['B', 'C', 'M', 'P'];
+  let list4 = ['F', 'H', 'V', 'W', 'Y'];
+  let list5 = ['K'];
+  let list8 = ['J', 'X'];
+  let list10 = ['Q', 'Z'];
+  
+  for (const char of upperWord){
+    if (list1.includes(char)){
+      score += 1;
+    } else if (list2.includes(char)){
+      score += 2; 
+    } else if (list3.includes(char)){
+      score += 3;
+    } else if (list4.includes(char)){
+      score += 4;
+    } else if (list5.includes(char)){
+      score += 5;
+    } else if (list8.includes(char)){
+      score += 8 ;
+    } else if (list10.includes(char)){
+      score += 10;
     }
   }
-  return true;
+  if (upperWord.length >= 7){
+    score += 8;
+  }
+  
+return score;
 };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
 
+// Implement this method for wave 4
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let winningWord = {}
+  let highestScore = 0
+  let highestStr = ''
+  // want to use a function closure 
+  let wordScore = scoreWord()
+  for (const word of words){
+    if (wordScore > highestScore){
+      let highestScore = wordScore;
+      let highestStr = word;
+  } else if (wordScore === highestScore){
+      if ((word.length < highestStr.length) && (highestStr.length !== 10)){
+        let highestStr = word;
+      } else if ((word.length === 10) && (highestStr.length !== 10)) {
+        let highestStr = word;
+      }
+  }
+}
+let result = (winningWord[highestStr] = highestScore);
+return result;
 };
