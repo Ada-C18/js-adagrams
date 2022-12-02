eead05af5be0b6f1792f00ee59543e0408e72426
Jessica <jessicaambrizmadrigal@gmail.com>
2022-12-01 21:50:37 -0800

    Modifed wave 1, just needs to add conditional of max 9 characters of same characters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,50 @@
+const letterPool = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let arrayletters = "";
+  for (let letter in letterPool) {
+    const amountOftimes = letterPool[letter];
+    for (let i = 0; i < amountOftimes; i++) {
+      arrayletters += letter;
+    }
+    let drawn = [];
+    let arrayLength = arrayletters.length;
+
+    for (let i = 0; i < 10; i++) {
+      letter = arrayletters.charAt(Math.floor(Math.random() * arrayLength));
+      // If letter is in array 9 times then do another loop
+
+      drawn.push(letter);
+    }
+    console.log(drawn);
+    return drawn;
+  }
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
