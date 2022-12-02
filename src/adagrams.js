b484041f249e6ee07ec11ae6d4f42c5281dd6dee
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2022-12-02 15:05:21 -0800

    Draw letter wave 01

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,55 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const LETTER_POOL = {
+    "A": 9,
+    "B": 2,
+    "C": 2,
+    "D": 4,
+    "E": 12,
+    "F": 2,
+    "G": 3,
+    "H": 2,
+    "I": 9,
+    "J": 1,
+    "K": 1,
+    "L": 4,
+    "M": 2,
+    "N": 6,
+    "O": 8,
+    "P": 2,
+    "Q": 1,
+    "R": 6,
+    "S": 4,
+    "T": 6,
+    "U": 4,
+    "V": 2,
+    "W": 2,
+    "X": 1,
+    "Y": 2,
+    "Z": 1
+  };
+
+  const theLetters = [];
+  for (const letter in LETTER_POOL) {
+    for (let i = 0; i < LETTER_POOL[letter]; i++) {
+      theLetters.push(letter)
+    }
+  }
+
+  const theHand = [];
+  for (let i = 0; i < 10; i++) {
+    const i = Math.floor(Math.random() * theLetters.length);
+    const letter = theLetters[i];
+    theHand.push(letter);
+    theLetters.splice(i,1);
+
+  }
+  return theHand
+
 };
+
+
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
