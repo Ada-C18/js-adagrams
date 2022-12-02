e78c1db8d52e744bfc65ae1beb76ba28af72fb8a
Yael <yaelechols@gmail.com>
2022-12-01 22:18:29 -0800

    Implements const lettersMap and drawLetters func

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,47 @@
+const lettersMap = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1
+}
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let lettersArr = [];
+
+  for (const key in lettersMap) {
+    lettersArr.push(...Array(lettersMap[key]).fill(key))
+  }
+
+  for (let i = lettersArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [lettersArr[i], lettersArr[j]] = [lettersArr[j], lettersArr[i]]
+  }
+
+  return lettersArr.slice(0, 10)
+
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
