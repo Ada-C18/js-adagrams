c42efb68bc44bc273d8d7b98e2178f99e84a829d
tiffany <tiffanyrogers.dev@gmail.com>
2022-12-02 12:01:09 -0500

    wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,6 +26,34 @@ let letterDict = {
   'Y': 2,
   'Z': 1
 }
+let scoreDict = {
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+  }
 export const drawLetters = () => {
   // Implement this method for wave 1
   let letterBank = []
@@ -73,9 +101,26 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 //     return True
 
+export const validateLetter = function charIsLetter(char) {
+  if (typeof char !== 'string') {
+    return false;
+  }
 
+  return /^[a-zA-Z]+$/.test(char);
+}
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  if (word.length === 0) return 0;
+  
+  let caseWord = word.toUpperCase();
+  let total = 0;
+  for (let i = 0; i < caseWord.length; i++){
+    if(validateLetter(word[i])== true){
+    let val = scoreDict[caseWord[i]]
+    total += val;}
+  }
+  if (word.length >= 7) total += 8
+  return total;
 };
 
 export const highestScoreFrom = (words) => {
