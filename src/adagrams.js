ae54d39d5dd34a36e67f9a453f6d6edc83180274
Tara <talsaidi@ucdavis.edu>
2022-12-02 12:47:45 -0800

    wave 1: incomplete solution, not taking into account letter distribution. passes all test but can only call each letter once. deciding to continue with array or to switch to frequency object literal

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,68 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let lettersInHand = [];
+  for (let i = 1; i <= 10; i++) {
+    let letterIndex = Math.floor(Math.random() * alphabet.length);
+    lettersInHand.push(alphabet.splice(letterIndex, 1).toString());
+  }
+  console.log(lettersInHand);
+  return lettersInHand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
