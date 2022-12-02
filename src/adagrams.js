6fa94405accdf95669d4214e93ce7ea0c8faf934
Ariel <gabriellaw15@gmail.com>
2022-12-01 18:37:53 -0800

    Add function 'scoreWord' which returns the score of a user's input word. Also completed test 'returns a score of 0 if given an empty input'

--- src/adagrams.js
+++ src/adagrams.js
@@ -90,7 +90,51 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  if (word.length === 0) {
+    return 0;
+  }
+  const wordUpper = word.toUpperCase();
+
+  const scoreChart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+
+  let score = 0;
+
+  for (let letter of wordUpper) {
+    score += scoreChart[letter];
+  }
+
+  if (wordUpper.length > 6) {
+    score += 8;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
