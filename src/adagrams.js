84c168850ea4c7f895f3f7a1dd8feb8fccfbec82
LexElena22 <103533828+LexElena22@users.noreply.github.com>
2022-12-01 22:07:54 -0800

    completed wave 03

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,7 @@ const letterPool = {
   Z: 1,
 };
 
+//---------------------------drawLetters----------------------------------
 export const drawLetters = () => {
   //makes copy of letter pool dict
   let newLetterPool = {};
@@ -50,6 +51,7 @@ export const drawLetters = () => {
   return lettersInHand;
 };
 
+//--------------------------usesAvailableLetters-------------------------
 export const usesAvailableLetters = (input, lettersInHand) => {
   const handDict = {};
   input = input.toUpperCase();
@@ -75,8 +77,30 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
+const points_dict = {
+  'A':1,'E':1,'I':1,'O':1,'U':1,'L':1,'N':1,'R':1,'S':1,'T':1,
+  'D':2,'G':2,
+  'B':3,'C':3,'M':3,'P':3,
+  'F':4,'H':4,'V':4,'W':4,'Y':4,
+  'K':5,
+  'J':8,'X':8,
+  'Q':10,'Z':10
+}
+
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let finalScore = 0;
+  let currentScore = 0;
+  word = word.toUpperCase();
+
+  if (word.length >= 7) {
+    finalScore = finalScore + 8;
+  }
+
+  for (const letter of word) {
+    currentScore = points_dict[letter];
+    finalScore += currentScore;
+  }
+  return finalScore;
 };
 
 export const highestScoreFrom = (words) => {
