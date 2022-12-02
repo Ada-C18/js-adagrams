de1fcb8821e10172255fa466b88ccc8b971a1113
Liat <liathirsh@gmail.com>
2022-12-01 22:36:38 -0500

    Completed Wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,58 @@
+// import { _ } from "core-js";
+// import { keys } from "core-js/core/array";
+// import { random } from "core-js/core/number";
+
+const LETTERPOOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+export const createLetterPool = () => {
+  const letterPoolList = [];
+  for (const [letter, value] of Object.entries(LETTERPOOL)) {
+    letterPoolList.push(...letter.repeat(value));
+  }
+  return letterPoolList;
+};
+
+export const findRandomLetter = (allLetters) => {
+  let randomIndex = Math.floor(Math.random() * allLetters.length);
+  return allLetters[randomIndex];
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let allLetters = createLetterPool(LETTERPOOL);
+  let hand = [];
+  for (let i = 0; i < 10; i++) {
+    let randomIndex = Math.floor(Math.random() * allLetters.length);
+    hand.push(allLetters[randomIndex]);
+    allLetters.splice(randomIndex, 1);
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
