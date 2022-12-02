542a924087457dc1cf08e640000f104b61800ee2
Ariel <gabriellaw15@gmail.com>
2022-12-01 21:05:25 -0800

    Deleted comments

--- src/adagrams.js
+++ src/adagrams.js
@@ -29,34 +29,12 @@ const pool = {
   Z: 1,
 };
 
-// const letters_pool = [
-//   'a', 'b', 'c', 'd',
-//   'e', 'f', 'g', 'h',
-//   'i', 'j', 'k', 'l',
-//   'm', 'n', 'o', 'p',
-//   'q', 'r', 's', 't',
-//   'u', 'v', 'w', 'x',
-//   'y', 'z'
-// ];
-
-// const freq_pool = [
-//   9, 2, 2, 4,
-//   12, 2, 3, 2,
-//   9, 1, 1, 4,
-//   2, 6, 8, 2,
-//   1, 6, 4, 6,
-//   4, 2, 2, 1,
-//   2, 1
-// ];
-
 export const drawLetters = () => {
-  // Implement this method for wave 1
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let hand = [];
   let hand_freq = {};
-  //while number of letters in hand is less than 10
+
   while (hand.length < 10) {
-    //choose a random letter from pool
     let random_int = Math.floor(Math.random() * 26);
     let random_letter = characters.charAt(random_int);
 
@@ -71,8 +49,6 @@ export const drawLetters = () => {
     }
   }
 
-  //make sure letter isn't over done
-  //add to hand (array)
   return hand;
 };
 
