f61c794e7d004f463e6a42e48aeecc98a83d4fe8
Younghee <kwak.younghee@gmail.com>
2022-12-03 14:49:25 -0800

    feat: create new function scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const LETTER_VALUES = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   /*
   (1-1) create letter bank from LETTER_POOL
@@ -70,7 +99,24 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  /*
+  (1) letter in word
+  (2) get the value from LETTER_VALUES (in dict)
+  (3) add each value to score
+  (4) if length of word is bigger than 6, add 8 points more
+  */
+
+  let score = 0;
+  word = word.toUpperCase();
+  for (let letter of word) {
+    score += LETTER_VALUES[letter];
+  }
+
+  if (word.length > 6) {
+    score += 8;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
