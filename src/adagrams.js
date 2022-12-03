eed1ee548428d0208f59aa6b817748dfd28525a1
Neida <neida@duck.com>
2022-12-02 21:21:12 -0500

    completed usesAvailableLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,36 +1,9 @@
-const weightedRandom = (items) => {
-  /*
-  Input: an object
-  Output: a weighted random item chosen from the object
-  */
-  const cummulativeWeights = [];
-  const itemKeys = Object.keys(items);
-  const itemValues = Object.values(items);
-
-  for (let i = 0; i < itemValues.length; i++) {
-    cummulativeWeights[i] = itemValues[i] + (cummulativeWeights[i - 1] || 0);
-  }
-
-  const randomNumber =
-    Math.random() * cummulativeWeights[cummulativeWeights.length - 1];
-
-  for (let j = 0; j < cummulativeWeights.length; j++) {
-    if (cummulativeWeights[j] >= randomNumber) {
-      return itemKeys[j];
-    }
-  }
-};
-
-Object.values = function (obj) {
-  return Object.keys(obj).map((key) => obj[key]);
-};
-
 export const drawLetters = () => {
   /*
   Input: none
   Output: array of 10 strings, where each string contains 1 letter
   */
-  const letterPool = {
+  const LETTER_POOL = {
     A: 9,
     B: 2,
     C: 2,
@@ -62,17 +35,33 @@ export const drawLetters = () => {
   const handOfLetters = [];
 
   while (handOfLetters.length < 10) {
-    const randomLetter = weightedRandom(letterPool);
-    if (letterPool[randomLetter] != 0) {
+    const randomLetter = weightedRandom(LETTER_POOL);
+    if (LETTER_POOL[randomLetter] != 0) {
       handOfLetters.push(randomLetter);
-      letterPool[randomLetter] -= 1;
+      LETTER_POOL[randomLetter] -= 1;
     }
   }
   return handOfLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  /*
+  Input: two parameters: input, which is a string, and lettersHand, which is an array of strings
+  Output: returns true is all letters in input are present in letterHand in the right quantities, or false if they are not
+  */
+  const usedLetters = [];
+  for (let i = 0; i < input.length; i++) {
+    const currentLetter = input[i].toUpperCase();
+    if (!lettersInHand.includes(currentLetter)) {
+      return false;
+    } else {
+      usedLetters.push(currentLetter);
+      lettersInHand.splice(lettersInHand.indexOf(currentLetter), 1);
+    }
+  }
+  if (usedLetters.length === input.length) {
+    return true;
+  }
 };
 
 export const scoreWord = (word) => {
@@ -81,4 +70,32 @@ export const scoreWord = (word) => {
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+};
+
+// helper functions
+const weightedRandom = (items) => {
+  /*
+  Input: an object
+  Output: a randomly chosen item from the object keys, based on the weightings in the object values
+  */
+  const cummulativeWeights = [];
+  const itemKeys = Object.keys(items);
+  const itemValues = Object.values(items);
+
+  for (let i = 0; i < itemValues.length; i++) {
+    cummulativeWeights[i] = itemValues[i] + (cummulativeWeights[i - 1] || 0);
+  }
+
+  const randomNumber =
+    Math.random() * cummulativeWeights[cummulativeWeights.length - 1];
+
+  for (let j = 0; j < cummulativeWeights.length; j++) {
+    if (cummulativeWeights[j] >= randomNumber) {
+      return itemKeys[j];
+    }
+  }
+};
+
+Object.values = function (obj) {
+  return Object.keys(obj).map((key) => obj[key]);
 };
