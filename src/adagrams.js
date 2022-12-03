bb4f75bd367b7daa66e31fe3f050a6cc190d12c9
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-12-03 10:17:47 -0800

    Implement scoreWord function and completed scoreWord test for if empty input was received.

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const SCORE_CHART = {
+  'A': 1, 
+  'B': 3, 
+  'C': 3, 
+  'D': 2, 
+  'E': 1, 
+  'F': 4, 
+  'G': 2, 
+  'H': 4, 
+  'I': 1, 
+  'J': 8, 
+  'K': 5, 
+  'L': 1, 
+  'M': 3, 
+  'N': 1, 
+  'O': 1, 
+  'P': 3, 
+  'Q': 10, 
+  'R': 1, 
+  'S': 1, 
+  'T': 1, 
+  'U': 1, 
+  'V': 4, 
+  'W': 4, 
+  'X': 8, 
+  'Y': 4, 
+  'Z': 10
+}
+
 const getRandomKey = (obj) => {
   const keys = Object.keys(obj);
   return keys[Math.floor(Math.random() * keys.length)];
@@ -58,9 +87,16 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
+export const scoreWord = (word) => {
+  let score = 0
+  if (word.length >= 7) {
+    score += 8;
+  }
+  for (let letter of word.toUpperCase()) {
+    score += SCORE_CHART[letter];
+  }
+  return score
+};
 
 // export const highestScoreFrom = (words) => {
 //   // Implement this method for wave 4
