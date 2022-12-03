7b0a2fd5a9624e7e095b4cdc4a3763f1a0d10092
dainiz <dainiz.almazan@gmail.com>
2022-12-02 19:06:43 -0800

    Implements scoreWord and refactors to capitalize arguments

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const SCORE_CHART = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const generateLetterPool = () => {
   const letterPool = [];
   const letterQuantities = Object.entries(LETTER_POOL);
@@ -50,6 +79,7 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
+  const capitalInput = input.toUpperCase();
   const letterQuantities = {};
   lettersInHand.forEach((letter) => {
     if (letter in letterQuantities) {
@@ -58,7 +88,7 @@ export const usesAvailableLetters = (input, lettersInHand) => {
       letterQuantities[letter] = 1;
     }
   });
-  for (let character of Array.from(input)) {
+  for (let character of Array.from(capitalInput)) {
     let isPresent = Object.keys(letterQuantities).includes(character);
     if (isPresent) {
       letterQuantities[character] -= 1;
@@ -73,7 +103,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const capitalWord = word.toUpperCase();
+  let score = 0;
+  Array.from(capitalWord).forEach((letter) => {
+    score += SCORE_CHART[letter];
+  });
+  if (word.length >= 7) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
