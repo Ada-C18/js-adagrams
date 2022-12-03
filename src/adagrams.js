731200bfcad39336bd986b1b4c0e83f94dc76b33
takhat <takhat014@gmail.com>
2022-12-03 12:12:28 -0800

    refactored wave 1 and wave 2 code to include helper functions

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,18 +1,15 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
   //1. Convert LETTERPOOL INTO LETTERARRAY
-  const LETTERARRAY = [];
-  for (const key in LETTERPOOL) {
-    for (let i = 0; i < LETTERPOOL[key]; i++) {
-      LETTERARRAY.push(key);
-    }
-  }
-  //2. Draw 10 letters and output them in an array
-  let deepCopyLETTERPOOL = JSON.parse(JSON.stringify(LETTERPOOL));
+  const LETTERARRAY = convertFreqDictToArr(LETTERPOOL);
+  //2. Make a deepcopy of LETTERPOOL dict, which can be modified as letters are drawn
+  let deepCopyLETTERPOOL = deepCopyDict(LETTERPOOL);
+  //3. Initialize an output array that will contain 10 letters drawn
   let outputArray = [];
+  //4. Draw 10 letters and make sure a letter is not repeated past max limit.
+  //(i.e. its Frequency or value in LETTERPOOL dict).
   while (outputArray.length < 10) {
-    const drawnLetter =
-      LETTERARRAY[Math.floor(Math.random() * LETTERARRAY.length)];
+    const drawnLetter = randomDraw(LETTERARRAY);
     if (deepCopyLETTERPOOL[drawnLetter] > 0) {
       outputArray.push(drawnLetter);
       deepCopyLETTERPOOL[drawnLetter] -= 1;
@@ -20,26 +17,59 @@ export const drawLetters = () => {
   }
   return outputArray;
 };
+// Helper method for Wave 1
+const convertFreqDictToArr = (freqDict) => {
+  const outputArr = [];
+  for (const key in freqDict) {
+    for (let i = 0; i < freqDict[key]; i++) {
+      outputArr.push(key);
+    }
+  }
+  return outputArr;
+};
+// Helper method for Wave 1
+const randomDraw = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// Helper method for Wave 1
+const deepCopyDict = (dict) => {
+  return JSON.parse(JSON.stringify(dict));
+};
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
   // - Returns `true` if every letter in the `input` word is available (in the right quantities) in the `lettersInHand`
   // - Returns `false` if not; if there is a letter in `input` that is not present in the `lettersInHand` or has too much of compared to the `lettersInHand`
-  const handDict = {};
-  for (const letter of lettersInHand) {
-    if (handDict[letter]) {
-      handDict[letter] += 1;
+
+  //1. convert lettersInHAnd to a frequency dictionary, so that frequency
+  //can be modified as letters are read.
+  const handDict = convertArrToDictWithFreqAsVal(lettersInHand);
+  //2. Match each letter in "Input" to handDict keys and make sure its present in
+  //the right quantities (values of dict keys).
+  return isSubset(input, handDict);
+};
+//Helper function for wave 2
+const convertArrToDictWithFreqAsVal = (arr) => {
+  const output_dict = {};
+  for (const elem of arr) {
+    if (output_dict[elem]) {
+      output_dict[elem] += 1;
     } else {
-      handDict[letter] = 1;
+      output_dict[elem] = 1;
     }
   }
-  for (const letter of input) {
-    if (!handDict[letter]) {
+  return output_dict;
+};
+//Helper function for wave 2
+const isSubset = (arr, dict) => {
+  for (const letter of arr) {
+    if (!dict[letter]) {
       return false;
-    } else if (handDict[letter] === 0) {
+    } else if (dict[letter] === 0) {
       return false;
     } else {
-      handDict[letter] -= 1;
+      dict[letter] -= 1;
     }
   }
   return true;
