99b20555d9a51b3b29a8526f1941cc1773bd883e
Soleil <soleilherring@gmail.com>
2022-12-04 11:11:51 -0800

    finished wave 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,8 +27,6 @@ export const drawLetters = () => {
     M: 2,
     Z: 1,
   };
-  // Implement this method for wave 1
-  // choose letter randomly from letters object. If chosen decrease it
 
   // make array to hold letters
   let lettersArr = [];
@@ -55,10 +53,6 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-
-  //  version 1
-
   for (let i = 0; i < input.length; i++) {
     if (!lettersInHand.includes(input[i])) {
       return false;
@@ -69,20 +63,6 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
   return true;
 };
-
-//  version 2
-
-//   for (let i = 0; i < input.length; i++) {
-//     if (lettersInHand.includes(input[i])) {
-//       lettersInHand.splice((lettersInHand.indexOf(input[i]), 1));
-//       // console.log(lettersInHand[lettersInHand.indexOf(input[i])]);
-//     } // if (!lettersInHand.includes(input[i])) {
-//     //   return false;
-//     // }
-//   }
-
-//   console.log(lettersInHand);
-// };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
@@ -131,7 +111,6 @@ export const scoreWord = (word) => {
 
   // if valid input:
   word = word.toUpperCase().split("");
-  console.log(word);
   let total = 0;
 
   // get total points
@@ -143,6 +122,85 @@ export const scoreWord = (word) => {
   return total;
 };
 
-// export const highestScoreFrom = (words) => {
-//   // Implement this method for wave 4
-// };
+export const highestScoreFrom = (words) => {
+  // PREP
+  // P - words list aka array of strings
+  // R- return object with word and score
+  // Example: ['cat', 'coyote'] --> {word: coyote, score: 11}
+  // pseudocode
+  // 1. create list to hold words and their scores
+  // 2. use scoreWord as helper function to calculate each words score and if word has highest score add it to object
+  // if tie: word has 10 letters or more, it is winner else smaller word is winner
+  //   multiple words with same points/word len, choose first word
+  const wordScores = [];
+
+  for (const word of words) {
+    wordScores.push({ word: word, score: scoreWord(word) });
+  }
+  // ###########
+  //  refactoring pt 2
+  // ###########
+  const maxScore = Math.max(...wordScores.map((wordObj) => wordObj.score));
+
+  const maxScoreList = wordScores.filter(
+    (wordObj) => wordObj.score === maxScore
+  );
+  const winner = maxScoreList.reduce((prev, current) =>
+    prev.word.length >= 10 ||
+    (prev.word.length < current.word.length && !(current.word.length === 10))
+      ? prev
+      : current
+  );
+  return winner;
+
+  // ##############
+  // refactoring
+  // ################
+
+  // const maxScore = Math.max(...wordScores.map((wordObj) => wordObj.score));
+
+  // const maxScoreList = wordScores.filter(
+  //   (wordObj) => wordObj.score === maxScore
+  // );
+
+  // if (maxScoreList.length === 1) {
+  //   return maxScoreList[0];
+  // } else {
+  //   const winner = maxScoreList.reduce((prev, current) =>
+  //     prev.word.length >= 10 ||
+  //     (prev.word.length < current.word.length && !(current.word.length === 10))
+  //       ? prev
+  //       : current
+  //   );
+  //   return winner;
+  // }
+
+  // ###############
+  // correct
+  // #############
+
+  // // let highestScoreObj = { word: "", score: scoreWord("") };
+  // let tiedWords = [];
+
+  // // find max score
+  // const maxScore = Math.max(...wordScores.map((wordObj) => wordObj.score));
+
+  // // make tiedwords list
+  // for (const word of wordScores) {
+  //   if (word.score === maxScore) {
+  //     tiedWords.push(word);
+  //   }
+  // }
+
+  // if (tiedWords.length === 1) {
+  //   return tiedWords[0];
+  // } else {
+  //   const winner = tiedWords.reduce((prev, current) =>
+  //     prev.word.length >= 10 ||
+  //     (prev.word.length < current.word.length && !(current.word.length === 10))
+  //       ? prev
+  //       : current
+  //   );
+  //   return winner;
+  // }
+};
