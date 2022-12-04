10792bd75591c338c3acbe9153a9fccf1af5a18c
Soleil <soleilherring@gmail.com>
2022-12-04 12:54:26 -0800

    deleted pseudo code

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,3 +1,4 @@
+// Wave 1
 export const drawLetters = () => {
   const letters = {
     A: 9,
@@ -28,13 +29,10 @@ export const drawLetters = () => {
     Z: 1,
   };
 
-  // make array to hold letters
   let lettersArr = [];
 
-  // made copy of letters object
   let lettersCopy = JSON.parse(JSON.stringify(letters));
 
-  // // made array of keys
   const keys = Object.keys(letters);
 
   let i = 0;
@@ -52,6 +50,7 @@ export const drawLetters = () => {
   return lettersArr;
 };
 
+// Wave 2
 export const usesAvailableLetters = (input, lettersInHand) => {
   for (let i = 0; i < input.length; i++) {
     if (!lettersInHand.includes(input[i])) {
@@ -64,11 +63,11 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
+// Wave 3
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
   let letterPoints = {};
 
-  // all letters and points
+  // all letters and points; This was my attempt at playing with the attribute functionality
   letterPoints.A =
     letterPoints.E =
     letterPoints.I =
@@ -97,23 +96,13 @@ export const scoreWord = (word) => {
 
   letterPoints.Q = letterPoints.Z = 10;
 
-  // const keys = Object.keys(letters);
-
-  // make all letters uppercase in word
-  // for each letter in word access it's value in dictionary
-  // use reduce to sum up total points
-  // reduce syntax :sum = objects.reduce((accumulator, currentValue) => accumulator + currentValue.x,
-
-  // guard clause:
   if (word.length === 0) {
     return 0;
   }
 
-  // if valid input:
   word = word.toUpperCase().split("");
   let total = 0;
 
-  // get total points
   total = word.reduce((acc, letter) => acc + letterPoints[letter], 0);
 
   if (word.length >= 7) {
@@ -122,24 +111,14 @@ export const scoreWord = (word) => {
   return total;
 };
 
+// Wave 4
 export const highestScoreFrom = (words) => {
-  // PREP
-  // P - words list aka array of strings
-  // R- return object with word and score
-  // Example: ['cat', 'coyote'] --> {word: coyote, score: 11}
-  // pseudocode
-  // 1. create list to hold words and their scores
-  // 2. use scoreWord as helper function to calculate each words score and if word has highest score add it to object
-  // if tie: word has 10 letters or more, it is winner else smaller word is winner
-  //   multiple words with same points/word len, choose first word
   const wordScores = [];
 
   for (const word of words) {
     wordScores.push({ word: word, score: scoreWord(word) });
   }
-  // ###########
-  //  refactoring pt 2
-  // ###########
+
   const maxScore = Math.max(...wordScores.map((wordObj) => wordObj.score));
 
   const maxScoreList = wordScores.filter(
@@ -152,55 +131,4 @@ export const highestScoreFrom = (words) => {
       : current
   );
   return winner;
-
-  // ##############
-  // refactoring
-  // ################
-
-  // const maxScore = Math.max(...wordScores.map((wordObj) => wordObj.score));
-
-  // const maxScoreList = wordScores.filter(
-  //   (wordObj) => wordObj.score === maxScore
-  // );
-
-  // if (maxScoreList.length === 1) {
-  //   return maxScoreList[0];
-  // } else {
-  //   const winner = maxScoreList.reduce((prev, current) =>
-  //     prev.word.length >= 10 ||
-  //     (prev.word.length < current.word.length && !(current.word.length === 10))
-  //       ? prev
-  //       : current
-  //   );
-  //   return winner;
-  // }
-
-  // ###############
-  // correct
-  // #############
-
-  // // let highestScoreObj = { word: "", score: scoreWord("") };
-  // let tiedWords = [];
-
-  // // find max score
-  // const maxScore = Math.max(...wordScores.map((wordObj) => wordObj.score));
-
-  // // make tiedwords list
-  // for (const word of wordScores) {
-  //   if (word.score === maxScore) {
-  //     tiedWords.push(word);
-  //   }
-  // }
-
-  // if (tiedWords.length === 1) {
-  //   return tiedWords[0];
-  // } else {
-  //   const winner = tiedWords.reduce((prev, current) =>
-  //     prev.word.length >= 10 ||
-  //     (prev.word.length < current.word.length && !(current.word.length === 10))
-  //       ? prev
-  //       : current
-  //   );
-  //   return winner;
-  // }
 };
