49d9e4a3ef892d688081cd0d9d18564d8d0b57ac
Yun <ly.yun811@gmail.com>
2022-12-04 23:42:31 -0800

    worked on wave 2-4. Need to debug and fix loops and scores

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const letterPoolCount = {
   Z: 1,
 };
 
+const scoreChart = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
   const letterList = Object.keys(letterPoolCount);
@@ -50,12 +79,62 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  let inputCaps = input.toUpperCase();
+  const inputLetters = inputCaps.split("");
+  for (let letter in inputLetters) {
+    // const result = arrayToCount.filter(i => i === 2).length;
+    let handCount = lettersInHand.filter((i) => i === letter).length;
+    let inputCount = inputLetters.filter((i) => i === letter).length;
+    if (!lettersInHand.includes(letter) || handCount < inputCount) {
+      return false;
+    }
+    return true;
+  }
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let wordCaps = word.toUpperCase();
+  let score = 0;
+  if (wordCaps.length >= 7 && wordCaps.length <= 10) {
+    score += 8;
+  }
+  // if (wordCaps.length >= 7 && wordCaps.length <= 10) {
+  //   let score = 8;
+  // } else {
+  //   let score = 0;
+  // }
+  // console.log(score)
+  for (let letter of wordCaps) {
+    score += scoreChart[letter];
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+  let winningWord = words[0];
+  let winningScore = scoreWord(winningWord);
+
+  for (let word in words) {
+    if (scoreWord(word) > winningScore) {
+      winningWord = word;
+      winningScore = scoreWord(word);
+    } else if (scoreWord(word) === winningScore) {
+      if (word.length === 10 && winningWord.length !== 10) {
+        winningWord = word;
+        winningScore = scoreWord(word);
+      } else if (
+        word.length < winningWord.length &&
+        winningWord.length !== 10
+      ) {
+        winningWord = word;
+        winningScore = scoreWord(word);
+      }
+    }
+  }
+  let highestScore = {};
+  highestScore["word"] = winningWord;
+  highestScore["score"] = winningScore;
+  return highestScore;
 };
