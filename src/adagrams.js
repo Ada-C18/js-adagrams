57db1e204fa0299fbf34cfccf82c31336da28729
Amanda <amanda.scott17@gmail.com>
2022-12-04 19:33:20 -0800

    wave 3 and half of wave 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -47,13 +47,99 @@ for (let i = 0; i < 10; i++){
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let letterArray = []
+  for (const letter of input){
+    
+    letterArray.push(letter);
+  }
+    for (const letter of input) {
+    const letterCountInHand = lettersInHand.filter(x=>x===letter).length
+    const letterCountInInput = letterArray.filter(x=>x===letter).length
+    if (letterCountInInput === letterCountInHand){
+      continue;
+    } else {
+      return false;
+    }   
+}
+return true;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreChart = {
+   
+     A: 1,
+     E: 1,
+     I: 1, 
+     O: 1,
+     U: 1,
+     L: 1,
+     N: 1, 
+     R: 1,
+     S: 1, 
+     T: 1,
+     D: 2,
+     G: 2,
+     B: 3, 
+     C: 3,
+     M: 3, 
+     P: 3,
+     F: 4,
+     H: 4, 
+     V: 4, 
+     W: 4, 
+     Y: 4,
+     K: 5,
+     J: 8,
+     X: 8,
+     Q: 10,
+     Z: 10
+  }
+  let counter = 0
+  
+  if (word.length >= 7 && word.length < 11){
+    counter += 8
+  }
+  for (const letter of word) {
+    counter += scoreChart[letter.toUpperCase()]
+  }
+  return counter;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let winningWord = {
+    'word': 0,
+    'score': 0
+  }
+  let scoredWords = [];
+  let highest = 0;
+  for (const word of words){
+    let scores = scoreWord(word);
+    if (scores > highest){
+      highest = scores;
+      scoredWords.push(scores);
+      winningWord.word = word;
+      winningWord.score = scores; 
+    } 
+  
+    if (scores === highest && word.length && word.length === 10){
+      continue;
+    }      
+    if (scores === highest && word.length != 10){
+      if (winningWord.word.length === 10){
+      continue;
+      }
+      else if (word.length < winningWord.word.length){
+        winningWord.word = word;
+        
+      }
+    if (scores === highest && winningWord.word.length === 10){
+      break;
+    }
+    }
+    }
+  
+  console.log(winningWord.word.length);
+  console.log(winningWord.word);
+  console.log(winningWord);
+  return winningWord;
 };
