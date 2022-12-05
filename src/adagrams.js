8e4c61a99b060b4e6d0b811374087f59867e4fcd
Maggie <mjtice0@gmail.com>
2022-12-04 18:58:38 -0700

    wave two tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -4,7 +4,33 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+
+  const wordList = input.split("");
+
+  //initialize empty dictionary/object
+
+  let letterCount = {};
+
+  //loop through word and conver to uppercase()
+  //if letter not in the letter count dictionary add it
+  for (letter of wordList) {
+    if (!letterCount[letter]) {
+      letterCount[letter] = 1;
+    } else {
+      letterCount[letter]++;
+    }
+  }
+
+  var lCount = Object.keys(letterCount).length;
+
+  if (lCount > lettersInHand(letter)) {
+    return false;
+  } else {
+    return true;
+  }
 };
+
+// word.forEach(element => { upper.push(element.toUpperCase());
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
