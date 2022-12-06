722e40bd71fad9186b9227605d500d0632dbdba9
nina-tuyen tran <ninatuyentran@gmail.com>
2022-12-06 14:50:54 -0800

    Completed test wave02 and test wave03 through implementation of a usesAvailableLetters function and scoreWord function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,7 @@
+// Test Wave 01
+
 export const drawLetters = () => {
-  const alphabet_dict = {
+  const alphabetDict = {
   'A': 9, 
   'B': 2, 
   'C': 2, 
@@ -28,8 +30,8 @@ export const drawLetters = () => {
   'Z': 1
   };
   const letter_pool = [];
-  Object.keys(alphabet_dict).forEach(key => {
-    let value = alphabet_dict[key];
+  Object.keys(alphabetDict).forEach(key => {
+    let value = alphabetDict[key];
     for (let i = 0; i < value; i++) {
       letter_pool.push(key)
     }
@@ -44,18 +46,66 @@ export const drawLetters = () => {
   };
 
   return hand;
-
 };
 
+// Test Wave 02
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  for (let letter in input) {
+    if (letter in lettersInHand) {
+      return true;
+    }
+    else {
+      return false;
+    };
+  }
 };
+
+
+// Test Wave 03
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let scoreChart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    };
+  let totalScore = 0;
+  for (const character of word) {
+    let capLetter = character.toUpperCase();
+    let letterValue = scoreChart[capLetter];
+    totalScore += letterValue;
+  }
+  if (word.length > 7) {
+    totalScore += 8;
+  }
+  return totalScore;
 };
 
+
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+
 };
