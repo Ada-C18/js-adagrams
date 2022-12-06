933e137c384432b0084fc7d8bd8f3ffbc46a6818
Yael <yaelechols@gmail.com>
2022-12-05 21:12:15 -0800

    Implements scoreWord and rearranges all objects to be more readable

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,30 +1,13 @@
 const lettersMap = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1
+  A: 9, B: 2, C: 2, 
+  D: 4, E: 12, F: 2, 
+  G: 3, H: 2, I: 9, 
+  J: 1, K: 1, L: 4,
+  M: 2, N: 6, O: 8,
+  P: 2, Q: 1, R: 6, 
+  S: 4, T: 6, U: 4, 
+  V: 2, W: 2, X: 1,
+  Y: 2, Z: 1
 }
 
 export const drawLetters = () => {
@@ -56,7 +39,23 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreMap = {
+    A: 1, B: 3, C: 3,
+    D: 2, E: 1, F: 4,
+    G: 2, H: 4, I: 1,
+    J: 8, K: 5, L: 1,
+    M: 3, N: 1, O: 1,
+    P: 3, Q: 10, R: 1,
+    S: 1, T: 1, U: 1,
+    V: 4, W: 4, X: 8,
+    Y: 4, Z: 10,
+  }
+
+  word = word.toUpperCase().split('');
+  let score = word.reduce((score, letter) => score + scoreMap[letter], 0)
+  if (word.length >= 7 && word.length <= 10) score += 8;
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
