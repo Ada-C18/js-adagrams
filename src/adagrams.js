f00c8cfbf4d1132d898eb6222734fa20768f3ef9
Lindsey <lindseymarcea@icloud.com>
2022-12-05 22:17:17 -0500

    Passed Wave 1 tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,64 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const letterPool = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+
+  const letterHand = [];
+  const letterDistribution = [];
+
+  for (const letter in letterPool) {
+    for (let i = 0; i < letterPool[letter]; i++) {
+      letterDistribution.push(letter);
+    }
+  }
+
+  while (letterHand.length < 10) {
+    const randomIndex = Math.floor(Math.random() * letterDistribution.length);
+    const randomLetter = letterDistribution[randomIndex];
+
+    letterHand.push(randomLetter);
+    letterDistribution.splice(randomIndex, 1);
+  }
+  return letterHand;
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+// export const usesAvailableLetters = (input, lettersInHand) => {
+// const word = word.toUpperCase()
+// const listOfWords =
+// const frequencyInWord = {}
+// };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+// // input is
 
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-};
+// export const scoreWord = (word) => {
+//   // Implement this method for wave 3
+// };
+
+// export const highestScoreFrom = (words) => {
+//   // Implement this method for wave 4
+// };
