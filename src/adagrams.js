2736ff342c4ae130dc24e81f96537dd6e6547356
Elsje Slothower <elsje.slothower@gmail.com>
2022-12-05 21:22:36 -0800

    created usesAvailableLetters() and scoreWord()

--- src/adagrams.js
+++ src/adagrams.js
@@ -7,13 +7,32 @@ const letterDistribution = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
 
 const scoreChart = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],  
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z']
+  'A': 1,
+  'E': 1,
+  'I': 1,
+  'O': 1,
+  'U': 1,
+  'L': 1,
+  'N': 1,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'D': 2,
+  'G': 2,
+  'B': 3,
+  'C': 3,
+  'M': 3,
+  'P': 3,
+  'F': 4,
+  'H': 4,
+  'V': 4,
+  'W': 4,
+  'Y': 4,
+  'K': 5,
+  'J': 8,
+  'X': 8,
+  'Q': 10,
+  'Z': 10
 }
 
 export const drawLetters = () => {
@@ -24,17 +43,33 @@ export const drawLetters = () => {
     let randomIndex = Math.floor(Math.random() * letterPool.length);
     let randomLetter = letterPool[randomIndex];
     letterBank.push(randomLetter);
-    letterPool.splice(randomLetter, 1);
+    letterPool.splice(randomIndex, 1);
   }
   return letterBank;
   };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let letterBank = lettersInHand.slice();
+  for (let letter of input) {
+    if (letterBank.includes(letter)) {
+      letterBank.splice(letter, 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let totalScore = 0;
+  let letters = Array.from(word.toUpperCase());
+  for (let i=0; i < letters.length; i++) {
+    totalScore += scoreChart[letters[i]];
+  }
+  if (7 <= word.length < 11) {
+    totalScore += 8;
+  }
+  return totalScore;
 };
 
 export const highestScoreFrom = (words) => {
