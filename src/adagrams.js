69da7517b0af3eb90baa415cc895d148c160c1e1
Elsje Slothower <elsje.slothower@gmail.com>
2022-12-05 20:44:07 -0800

    simplified drawLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,51 +1,25 @@
-const letterDistribution = {
-  "A": 9,
-  "B": 2,
-  "C": 2,
-  "D": 4,
-  "E": 12,
-  "F": 2,
-  "G": 3,
-  "H": 2,
-  "I": 9,
-  "J": 1,
-  "K": 1,
-  "L": 4,
-  "M": 2,
-  "N": 6,
-  "O": 8,
-  "P": 2,
-  "Q": 1,
-  "R": 6,
-  "S": 4,
-  "T": 6,
-  "U": 4,
-  "V": 2,
-  "W": 2,
-  "X": 1,
-  "Y": 2,
-  "Z": 1
-}
+const letterDistribution = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 
+'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 
+'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 
+'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 
+'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 
+'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 
+'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
 
 const scoreChart = {
-  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],  
-  2: ["D", "G"],
-  3: ["B", "C", "M", "P"],
-  4: ["F", "H", "V", "W", "Y"],
-  5: ["K"],
-  8: ["J", "X"],
-  10: ["Q", "Z"]
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],  
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
 }
 
 export const drawLetters = () => {
-  let letterPool = [];
+  let letterPool = letterDistribution;
   let letterBank = [];
-  for (let i=letterDistribution.length; i === 0; i--) {
-    for (let j=0; j < i; j++) {
-      letterPool.push(letterDistribution[i]);
-    }
-  }
-
+  
   for (let i=0; i<10; i++) {
     let randomIndex = Math.floor(Math.random() * letterPool.length);
     let randomLetter = letterPool[randomIndex];
