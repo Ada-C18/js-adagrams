a9322698ea1ce8b8bd19bb6b77693d01ecb28d05
Kindra <kindra.kathleen@gmail.com>
2022-12-06 12:08:11 -0500

    completed scoreWord func. all tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const alphabet = [
   ["Z", 1],
 ];
 
+const scoreChart = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
+
 function createLetterPool(alphabet) {
   const letterPool = [];
   for (let i = 0; i < alphabet.length; ++i) {
@@ -67,7 +96,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const formattedWord = word.toUpperCase();
+  let score = 0;
+  for (let char of formattedWord) {
+    score += scoreChart[char];
+  }
+  if (word.length >= 7) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
