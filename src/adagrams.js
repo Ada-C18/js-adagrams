d4bf575659572607e8820683f52ac2260e220a89
Emily <sonemily+github@gmail.com>
2022-12-05 19:36:06 -0500

    Create highestScoreFrom that returns the word with the highest score.

--- src/adagrams.js
+++ src/adagrams.js
@@ -130,5 +130,22 @@ export const scoreWord = (word) => {
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let highestScore = 0;
+  let winningWord = null;
+
+  for (let word of words) {
+    if (scoreWord(word) > highestScore) {
+      highestScore = scoreWord(word);
+      winningWord = word;
+    } else if (scoreWord(word) === highestScore) {
+      if (word.length === winningWord.length || winningWord.length == 10) {
+        continue;
+      } else if (word.length === 10) {
+        winningWord = word;
+      } else if (word.length < winningWord.length) {
+        winningWord = word;
+      }
+    }
+  }
+  return { word: winningWord, score: highestScore };
 };
