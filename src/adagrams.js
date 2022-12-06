8dbea0afee96713afe14d7ca59d1cf1dfd0f4665
ivystrayed <ivystrayed@gmail.com>
2022-12-05 21:46:18 -0800

    started on wave 01, tests no longer work

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,4 +1,54 @@
+
+
+
 export const drawLetters = () => {
+  
+  const letterPool = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+  };
+
+  let letterList = Object.keys(letterPool);
+
+  for (const letter of letterList){
+    for (let i = 0; i < letterPool[letter]; i++){
+      letterList.push(letter);
+    }
+  }
+
+  let hand = [];
+  while (hand.length < 10){
+    let letterDrawn = letterPool[Math.floodsr(Math.random()*items.length)];
+    hand.push(letterDrawn);
+  }
+
+  return hand;
+
+
   // Implement this method for wave 1
 };
 
