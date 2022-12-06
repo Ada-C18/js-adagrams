b977357352fe4dac653cb8b0b4dc85dba02d7b44
Devin Taylor <devin.taylor122@gmail.com>
2022-12-05 20:37:59 -0800

    finished waves 2 and 3. all tests passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -117,11 +117,63 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let word = input.toUpperCase();
+  let copy = lettersInHand.slice();
+  let result = true;
+
+  for (let letter of word) {
+    if (copy.includes(letter)) {
+      let index = copy.indexOf(letter);
+      if (index > -1) {
+        copy.splice(index, 1);
+      }
+    } else {
+      result = false;
+    }
+  }
+  return result;
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const onePoint = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
+  const twoPoint = ["D", "G"];
+  const threePoint = ["B", "C", "M", "P"];
+  const fourPoint = ["F", "H", "V", "W", "Y"];
+  const fivePoint = ["K"];
+  const eightPoint = ["J", "X"];
+  const tenPoint = ["Q", "Z"];
+
+  let points = 0;
+
+  if (word === null || word === "") {
+    return points;
+  }
+
+  let upperWord = word.toUpperCase();
+
+  if (upperWord.length > 6) {
+    points += 8;
+  }
+
+  for (let letter of upperWord) {
+    if (onePoint.includes(letter)) {
+      points += 1;
+    } else if (twoPoint.includes(letter)) {
+      points += 2;
+    } else if (threePoint.includes(letter)) {
+      points += 3;
+    } else if (fourPoint.includes(letter)) {
+      points += 4;
+    } else if (fivePoint.includes(letter)) {
+      points += 5;
+    } else if (eightPoint.includes(letter)) {
+      points += 8;
+    } else if (tenPoint.includes(letter)) {
+      points += 10;
+    }
+  }
+
+  return points;
 };
 
 export const highestScoreFrom = (words) => {
