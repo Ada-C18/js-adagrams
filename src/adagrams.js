88460e28c73d93e2afc73b14adb106aa6ccb776d
Katherine Abrikian <kabrikian@gmail.com>
2022-12-06 16:31:29 -0500

    passed wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -111,8 +111,57 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
+const SCORECHART = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
+Object.freeze(SCORECHART);
+
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const bonusFloorLength = 7;
+  const scoreBonus = 8;
+
+  let score = 0;
+
+  if (word.length >= bonusFloorLength) {
+    score += scoreBonus;
+  }
+
+  let wordMap = makeWordMap(word);
+
+  let wordIter = wordMap[Symbol.iterator]();
+
+  for (let item of wordIter) {
+    let [letter, n] = item;
+    score += SCORECHART[letter] * n;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
