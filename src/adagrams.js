db12b67691dd3eb5cbda76ba796eeae47475cdff
checarina <checarina@gmail.com>
2022-12-05 19:10:19 -0800

    implemented scoreWord function

--- src/adagrams.js
+++ src/adagrams.js
@@ -60,7 +60,44 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreDict = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+
+    word = word.toUpperCase();
+    let wordScore = 0;
+    for (const letter of word) {
+      wordScore += scoreDict[letter];
+    }
+    if (7 <= word.length) {
+      wordScore += 8;
+    }
+  return wordScore;
 };
 
 export const highestScoreFrom = (words) => {
