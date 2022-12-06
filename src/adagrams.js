61938340baf99ab599a580439690dc31cdbf0062
Anna <annalordkemp@gmail.com>
2022-12-05 19:06:45 -0700

    completes usesAvailableLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -29,7 +29,7 @@ const LETTER_POOL = {
 
 
 export const drawLetters = () => { //wave 1
-  let letterArray = [];
+  let letterArray = []; //make this a helper function?? 
   for (const [key, value] of Object.entries(LETTER_POOL)) {
     for (let i = 0; i < value; i++) {
       letterArray.push(key)
@@ -37,7 +37,7 @@ export const drawLetters = () => { //wave 1
   }
 
   let resultArray = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) { //refactor to avoid magic nums??
     let index = Math.floor(Math.random() * 26);
     resultArray.push(letterArray[index]);
     letterArray.splice(index, 1);
@@ -46,8 +46,18 @@ export const drawLetters = () => { //wave 1
   return resultArray;
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+export const usesAvailableLetters = (input, lettersInHand) => { //wave 2
+  for (let i = 0; i < input.length; i++) {
+    let letter = input[i];
+    if (lettersInHand.includes(letter)) {
+      let letterIndex = lettersInHand.indexOf(letter);
+      lettersInHand.splice(letterIndex, 1);
+      }
+    else {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
