7ad64fb34e71889af9c2c6d186ce5e8ce03705c8
Anna <annalordkemp@gmail.com>
2022-12-05 19:51:21 -0700

    completes scoreWord function and completes test 'returns a score of 0 if given an empty input'

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const LETTER_SCORES = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+}
+
 
 export const drawLetters = () => { //wave 1
   let letterArray = []; //make this a helper function?? 
@@ -57,11 +86,19 @@ export const usesAvailableLetters = (input, lettersInHand) => { //wave 2
       return false;
     }
   }
-  return true;
+  return true; //refactor??? 
 };
 
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
+export const scoreWord = (word) => { //wave 3
+  let score = 0;
+  if (word.length >= 7) {
+    score += 8;
+  }
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i].toUpperCase();
+    score += LETTER_SCORES[letter];
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
