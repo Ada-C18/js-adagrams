375429c98dc8836d14e47d240f9cf939a1ad7366
Maggie <mjtice0@gmail.com>
2022-12-06 17:59:02 -0700

    all waves passing tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,40 +1,34 @@
-<<<<<<< HEAD
-
-
+//WAVE 1
 export const drawLetters = () => {
-  // Implement this method for wave 1
-=======
-const LETTER_POOL = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1,
-};
+  const LETTER_POOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
 
-export const drawLetters = () => {
->>>>>>> b2339a8457d2c5cc25f4c47b8179bfcd4f9db969
   let letterBank = [];
   let letterCopy = JSON.parse(JSON.stringify(LETTER_POOL));
   let keyList = Object.keys(letterCopy);
@@ -48,19 +42,8 @@ export const drawLetters = () => {
   }
   return letterBank;
 };
-
+//WAVE 2
 export const usesAvailableLetters = (input, lettersInHand) => {
-<<<<<<< HEAD
-  let letterCount = {};
-  //loop through input and add each letter to a new list, count how many times each letter occur
-  //count how many times each letter occurs in lettersInHand and compare
-
-  const letters = input.split("");
-
-  return letters.every((attemptedLetter) =>
-    lettersInHand.includes(attemptedLetter)
-  );
-=======
   let letters = input.toUpperCase();
 
   //for-of loop iterates over the values in an iterable object, such as an array.
@@ -79,57 +62,17 @@ export const usesAvailableLetters = (input, lettersInHand) => {
     }
   }
 
-  //calling the filter() method on the lettersInHand array returns an array containing
-  // only the occurrences of the letter in the lettersInHand array.
-  // The length of this array is then compared to the count variable.
-
-  // another way to write this code: for (const letter of Object.keys(letterCounts)) {const count = letterCounts[letter];
-  // rest of loop body goes here
-
   for (const [letter, count] of Object.entries(letterCounts)) {
     if (count > lettersInHand.filter((l) => l === letter).length) {
       return false;
     }
   }
-
   return true;
 };
 
-const SCORE_CHART = {
-  A: 1,
-  B: 3,
-  C: 3,
-  D: 2,
-  E: 1,
-  F: 4,
-  G: 2,
-  H: 4,
-  I: 1,
-  J: 8,
-  K: 5,
-  L: 1,
-  M: 3,
-  N: 1,
-  O: 1,
-  P: 3,
-  Q: 10,
-  R: 1,
-  S: 1,
-  T: 1,
-  U: 1,
-  V: 4,
-  W: 4,
-  X: 8,
-  Y: 4,
-  Z: 10,
->>>>>>> b2339a8457d2c5cc25f4c47b8179bfcd4f9db969
-};
-
+//WAVE 3
 export const scoreWord = (word) => {
-<<<<<<< HEAD
-  let score = 0;
-
-  let letterValues = {
+  const SCORE_CHART = {
     A: 1,
     B: 3,
     C: 3,
@@ -158,32 +101,20 @@ export const scoreWord = (word) => {
     Z: 10,
   };
 
-  word = word.toUpperCase();
-
-  for (let i = 0; i < word.length; i++) {
-    score += letterValues[word[i]];
-=======
-  if (word.length === 0) {
-    return 0;
->>>>>>> b2339a8457d2c5cc25f4c47b8179bfcd4f9db969
-  }
-
   let score = 0;
   const letters = word.toUpperCase();
   for (const letter of letters) {
     score += SCORE_CHART[letter] || 0;
   }
 
-
   if (letters.length >= 7) {
     score += 8;
   }
-
   return score;
 };
 
+//WAVE 4
 export const highestScoreFrom = (words) => {
-
   let winningWords = {};
   let winningScore = 0;
 
@@ -194,7 +125,6 @@ export const highestScoreFrom = (words) => {
       winningScore = score;
       winningWords["score"] = score;
       winningWords["word"] = word;
-      //if word score is the same as the winning score (there is a tie), return the one that is a length of 10
     } else if (score === winningScore) {
       if (winningWords["word"].length === 10) {
         return winningWords;
@@ -202,13 +132,11 @@ export const highestScoreFrom = (words) => {
       if (word.length === 10) {
         winningWords["score"] = score;
         winningWords["word"] = word;
-        //if neither of the words have a length of ten but are the same score, return the word that has the fewest letters
       } else if (word.length < winningWords["word"].length) {
         winningWords["score"] = score;
         winningWords["word"] = word;
       }
     }
   }
-
   return winningWords;
 };
