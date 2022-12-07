6dff0c891a8e964c2b993d81b816ed64be19a0bf
Jacqueline Hopkins <318jlh@gmail.com>
2022-12-07 02:30:11 -0800

    Wave 3: Implement scoreWord function

--- src/adagrams.js
+++ src/adagrams.js
@@ -64,8 +64,55 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
+
+// Has one parameter: word, which is a string of characters
+// Returns an integer representing the number of points
+// Each letter within word has a point value. The number of points of each letter is summed up to represent the total score of word
+// Each letter's point value is described in the table below
+// If the length of the word is 7, 8, 9, or 10, then the word gets an additional 8 points
+
+
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const letterValues ={
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+  
+  let score = 0;
+  const upperCaseWord = word.toUpperCase();
+  for (const letter of upperCaseWord) {
+  // word.forEach((letter) => {
+    score += letterValues[letter];
+  }
+  
+  if  (word.length === 7 || word.length === 8 || word.length === 9 || word.length === 10) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
