c64153a1afb24638d455b9d4f5ebe9e28a951ea8
Aretta Brown <aretta.brown@gmail.com>
2022-12-07 05:41:00 -0500

    passes wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,45 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const letterPool = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+  
+  
+
+  const letters = Object.keys(letterPool);
+  let hand = []
+  while (hand.length < 10) {
+    let letter = letters[Math.floor(Math.random() * letters.length)];
+    letterPool[letter] = letterPool[letter] - 1;
+    if (letterPool[letter] > 0) {
+      hand.push(letter)
+    }
+  }
+  return hand
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
