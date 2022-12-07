b7a85e89c8d792380de5ab396b54077e8ac96e59
Paje <ejap99@gmail.com>
2022-12-06 23:05:22 -0600

    tests not working

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,19 +1,42 @@
-
-const points = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1,
-'N': 1, 'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C':3, 'M': 3, 'P': 3,
-'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8, 
-'Q': 10, 'Z': 10}
+const points = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
 
 export const drawLetters = () => {
   // Implement this method for wave 1
 
-  const letterPool = 'AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ'
-  let tempPool = letterPool.map((x) => x)
+  const letterPool = 'AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ';
+  let tempPool = [...letterPool];
 
   let drawnLetters = [];
 
-  for (let i = 0; drawnLetters.length<10; i++) {
-    let letter = letterPool[Math.floor(Math.random() * letterPool.length)]
+  for (let i = 0; drawnLetters.length < 10; i++) {
+    let letter = letterPool[Math.floor(Math.random() * letterPool.length)];
     if (tempPool.includes(letter)) {
       tempPool = tempPool.replace(letter, '');
       drawnLetters.push(letter);
@@ -24,16 +47,16 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
-  let newList = lettersInHand.map((x)=>x)
-  let inputUpper = input.toUpperCase()
+  let newList = [...lettersInHand];
+  let inputUpper = input.toUpperCase();
 
   for (let i = 0; i < input.length; i++) {
     if (newList.includes(inputUpper[i])) {
-      const index = newList.indexOf(inputUpper[i])
-      newList.splice(index, 1)
+      const index = newList.indexOf(inputUpper[i]);
+      newList.splice(index, 1);
       continue;
     } else {
-      return false; 
+      return false;
     }
   }
   return true;
@@ -46,35 +69,35 @@ export const scoreWord = (word) => {
     return score;
   }
 
-  const wordUpper = word.toUpperCase()
+  const wordUpper = word.toUpperCase();
 
   for (let index in wordUpper) {
-    score += points[wordUpper[index]]
+    score += points[wordUpper[index]];
   }
   if (word.length >= 7) {
-    score += 8
+    score += 8;
   }
   return score;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
-  let highestWord = ''
-  let highestScore = 0
-  
+  let highestWord = '';
+  let highestScore = 0;
+
   for (let index in words) {
-    let score = scoreWord(words[index])
-      if (score > highestScore) {
-        highestScore = score
-        highestWord = words[index]
-      } else if (score == highestScore){
-        const highLen = highestWord.length;
-        const wordLen = words[index].length;
-        if (highLen != 10 && (wordLen == 10 || wordLen < highLen)) {
-          highestWord = words[index]
-          }
-        }
+    let score = scoreWord(words[index]);
+    if (score > highestScore) {
+      highestScore = score;
+      highestWord = words[index];
+    } else if (score == highestScore) {
+      const highLen = highestWord.length;
+      const wordLen = words[index].length;
+      if (highLen != 10 && (wordLen == 10 || wordLen < highLen)) {
+        highestWord = words[index];
       }
-  let wordScore = {'score': highestScore, 'word': highestWord}
-  return wordScore
-  };
+    }
+  }
+  let wordScore = { score: highestScore, word: highestWord };
+  return wordScore;
+};
