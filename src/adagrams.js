bb466e4c520d4e21302602bf7441b42625f5ef59
Jacqueline Hopkins <318jlh@gmail.com>
2022-12-07 10:00:26 -0800

    src/adagrams.js

--- src/adagrams.js
+++ src/adagrams.js
@@ -116,16 +116,6 @@ export const scoreWord = (word) => {
 };
 
 
-// Implement the function named highestScoreFrom in src/adagrams.js. This method should have the following properties:
-
-// Has one parameter: words, which is an array of strings
-// Returns a single object that represents the data of a winning word and its score. The object should have the following keys:
-// word, whose value is a string of a word
-// score, whose value is the score of that word
-// In the case of tie in scores, use these tie-breaking rules:
-// prefer the word with the fewest letters...
-// ...unless one word has 10 letters. If the top score is tied between multiple words and one is 10 letters long, choose the one with 10 letters over the one with fewer tiles
-// If the there are multiple words that are the same score and the same length, pick the first one in the supplied list
 export const highestScoreFrom = (words) => {
 // intialize score/value object
 const highestScoringWord = {};
