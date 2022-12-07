45db3dda649e5d17e9b26684892b79d7c01b8f4d
hoku mcangus <mcangusohana2@gmail.com>
2022-12-06 22:39:40 -0800

    Finish 3 waves

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,45 +26,88 @@ const letterPool = {
   Y: 2,
   Z: 1,
 };
-const pointSystem = {
-  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-  2: ['D', 'G'],
-  3: ['B', 'C', 'M', 'P'],
-  4: ['F', 'H', 'V', 'W', 'Y'],
-  5: ['K'],
-  8: ['J', 'X'],
-  10: ['Q', 'Z'],
-};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
   const playersHand = [];
-  for (let letter in letterPool) {
-    for (let i = 0; i < letterPool[letter]; i++) {
-      //while i is < the frequency of the letter
-      playersHand.push(letter);
+  const otherLetters = [];
+  const copyOfLetterPool = {...letterPool};
+  for (let letter in copyOfLetterPool) {
+    for (let i = 0; i < copyOfLetterPool[letter]; i++) {
+      otherLetters.push(letter);
     }
   }
-  const shuffleLetters = (playersHand) => {
-    for (let i = playersHand.length - 1; i > 0; i--) {
-      //assign len of hand - 1 to i, while i is > 0 loop (-1) from i each iteration
-      let j = Math.floor(Math.random() * (i + 1)); 
-      [playersHand[i], playersHand[j]] = [playersHand[j], playersHand[i]]; //the two letters switch their index
-    }
-    return playersHand.slice(0, 10); //after shuffle the first 10 are returned
-  };
-  return shuffleLetters(playersHand);
+  for (let i = 0; i < 10; i++) {
+    let index = Math.floor(Math.random() * otherLetters.length);
+    // add to playersHand
+    playersHand.push(otherLetters[index]);
+    // remove from other letters
+    otherLetters.splice(index, 1);
+  }
+  return playersHand;
 };
-
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-   // Implement this method for wave 2
-
-  
+  // Implement this method for wave 2
+  const lengthOfHand = input.length;
+  let lettersInHandCopy = [...lettersInHand];
+  for (let i = 0; i < lengthOfHand; i++) {
+    let letter = input[i];
+    if (lettersInHandCopy.includes(letter)) {
+      let j = lettersInHandCopy.indexOf(letter);
+      lettersInHandCopy.splice(j, 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
-// export const scoreWord = (word) => {
-//   // Implement this method for wave 3
-// };
+export const scoreWord = (word) => {
+  //Implement this method for wave 3
+  const pointSystem = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
 
-// export const highestScoreFrom = (words) => {};
-// Implement this method for wave 4
+  let score = 0;
+  const capsWord = word.toUpperCase();
+  for (let letter of capsWord) {
+    if (letter in pointSystem) {
+      score += pointSystem[letter];
+    }
+  }
+  if (word.length >= 7) {
+    score += 8;
+  }
+  return score;
+};
+
+export const highestScoreFrom = (words) => {
+  // Implement this method for wave 4
+  
+};
