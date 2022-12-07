22ce0f33a3672095d49136bc0af5c5b8ccc51490
Cristal <cbcodee@gmail.com>
2022-12-06 17:48:52 -0800

    completed wave 03 and tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -60,6 +60,47 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const lettersDict = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 9,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+
+  let sum = 0;
+
+  for (let letter of word) {
+    let value = lettersDict[letter.toUpperCase()];
+    sum += value;
+  }
+  if (word.length > 6) {
+    sum += 8;
+  } else if (word.length == "") {
+    return 0;
+  }
+  return sum;
 };
 
 export const highestScoreFrom = (words) => {
