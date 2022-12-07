e6e76b0c95e026f273c1ecdb2d15177a71dfd003
Yael <yaelechols@gmail.com>
2022-12-07 00:16:34 -0800

    Moves scoreMap global variable to top of file

--- src/adagrams.js
+++ src/adagrams.js
@@ -10,6 +10,18 @@ const lettersMap = {
   Y: 2, Z: 1
 }
 
+const scoreMap = {
+  A: 1, B: 3, C: 3,
+  D: 2, E: 1, F: 4,
+  G: 2, H: 4, I: 1,
+  J: 8, K: 5, L: 1,
+  M: 3, N: 1, O: 1,
+  P: 3, Q: 10, R: 1,
+  S: 1, T: 1, U: 1,
+  V: 4, W: 4, X: 8,
+  Y: 4, Z: 10,
+}
+
 export const drawLetters = () => {
   let lettersArr = [];
   let hand = [];
@@ -39,18 +51,6 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  const scoreMap = {
-    A: 1, B: 3, C: 3,
-    D: 2, E: 1, F: 4,
-    G: 2, H: 4, I: 1,
-    J: 8, K: 5, L: 1,
-    M: 3, N: 1, O: 1,
-    P: 3, Q: 10, R: 1,
-    S: 1, T: 1, U: 1,
-    V: 4, W: 4, X: 8,
-    Y: 4, Z: 10,
-  }
-
   word = word.toUpperCase().split('');
   let score = word.reduce((score, letter) => score + scoreMap[letter], 0)
   if (word.length >= 7 && word.length <= 10) score += 8;
@@ -82,6 +82,6 @@ export const highestScoreFrom = (words) => {
 
   const smallestWord = wordObjs.find(
     (word) => word.length === (Math.min(...scoreFilteredWords.map((word) => word.length))));
-    
+
   return { word: smallestWord.word, score: smallestWord.score };
 };
