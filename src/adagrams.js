093411134aa5f0934adf4b0268db728d87286fda
Masha <iiureva.mariia@gmail.com>
2022-12-07 00:40:58 -0800

    Wave 3 and 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const letterPool = {
   Z: 1,
 };
 
+const letterPoints = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 let letterPoolArray = [];
 Object.keys(letterPool).forEach((letter) => {
   for (let i = 0; i < letterPool[letter]; i++) {
@@ -65,8 +94,60 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let wordUpperCase = word.toUpperCase();
+  let wordScore = 0;
+  for (let i = 0; i < wordUpperCase.length; i++) {
+    wordScore += letterPoints[wordUpperCase[i]];
+  }
+  if (wordUpperCase.length > 6) {
+    wordScore += 8;
+  }
+  return wordScore;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+
+  let wordToScoreDict = {};
+  let wordLengthDict = {};
+  let currentScore = scoreWord(words[0]);
+
+  // let result = {
+  //   word: "",
+  //   score: 0
+  // }
+
+  for (const word of words) {
+    wordToScoreDict[word] = scoreWord(word);
+    wordLengthDict[word.length] = word;
+  }
+
+  for (const word of words) {
+    // determine highest score
+    if (wordToScoreDict[word] > currentScore) {
+      currentScore = wordToScoreDict[word];
+    }
+  }
+  let maxScore = currentScore;
+  let currentMinLength = 9;
+
+  let result = {};
+
+  for (const word of words) {
+    if (wordToScoreDict[word] === maxScore) {
+      if (word.length === 10) {
+        return {
+          word: word,
+          score: wordToScoreDict[word],
+        };
+      } else if (word.length < currentMinLength) {
+        currentMinLength = word.length;
+        result = {
+          word: word,
+          score: wordToScoreDict[word],
+        };
+      }
+    }
+  }
+  return result;
 };
