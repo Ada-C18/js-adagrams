121c8ef0f60fabb6e76b2040a132c006106a9aea
Aretta Brown <aretta.brown@gmail.com>
2022-12-07 07:14:49 -0500

    passes wave 2 and 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -31,23 +31,77 @@ export const drawLetters = () => {
   
 
   const letters = Object.keys(letterPool);
-  let hand = []
+  let hand = [];
   while (hand.length < 10) {
     let letter = letters[Math.floor(Math.random() * letters.length)];
     letterPool[letter] = letterPool[letter] - 1;
     if (letterPool[letter] > 0) {
-      hand.push(letter)
-    }
-  }
-  return hand
+      hand.push(letter);
+    };
+  };
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  const testWord = input.toUpperCase();
+  let hand = [];
+
+  for(let letter of lettersInHand) {
+    hand.push(letter);
+  };
+
+  for(let characters of testWord){
+    if (hand.includes(characters)) {
+      hand.splice(characters, 1);
+    } else {
+      return false;
+    };
+  };
+
+  return true
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreChart= {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    };
+
+  let score = 0;
+  const scoreWord = word.toUpperCase();
+
+  for (let letter of scoreWord) {
+        score += scoreChart[letter]
+  }
+
+  if (word.length >= 7) {
+        score += 8
+  }
+    return score
 };
 
 export const highestScoreFrom = (words) => {
