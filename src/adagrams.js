dc9a0c2da695fbfd3ef435a313ae8eea586717b4
Carina <checarina@gmail.com>
2022-12-08 15:34:16 -0800

    deleted unnecessary line from usesAvailableLetters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,6 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
-    const LETTER_POOL = {
+  const LETTER_POOL = {
     A: 9,
     B: 2,
     C: 2,
@@ -35,13 +35,13 @@ export const drawLetters = () => {
       letterList.push(letter);
     }
   }
-    
-  const hand = []
+
+  const hand = [];
   for (let i = 0; i < 10; i++) {
     let rand_index = Math.floor(Math.random() * letterList.length);
     hand.push(letterList[rand_index]);
-    letterList.splice(rand_index, 1); 
-  };
+    letterList.splice(rand_index, 1);
+  }
   return hand;
 };
 
@@ -49,8 +49,7 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   const word = input.toUpperCase();
   let handCopy = lettersInHand.slice();
   for (const letter of word) {
-    if (!(handCopy.includes(letter))) {
-      handCopy = lettersInHand.slice();
+    if (!handCopy.includes(letter)) {
       return false;
     }
     let index = handCopy.indexOf(letter);
@@ -61,42 +60,42 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   const scoreDict = {
-    'A': 1, 
-    'B': 3, 
-    'C': 3, 
-    'D': 2, 
-    'E': 1, 
-    'F': 4, 
-    'G': 2, 
-    'H': 4, 
-    'I': 1, 
-    'J': 8, 
-    'K': 5, 
-    'L': 1, 
-    'M': 3, 
-    'N': 1, 
-    'O': 1, 
-    'P': 3, 
-    'Q': 10, 
-    'R': 1, 
-    'S': 1, 
-    'T': 1, 
-    'U': 1, 
-    'V': 4, 
-    'W': 4, 
-    'X': 8, 
-    'Y': 4, 
-    'Z': 10
-    }
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
 
-    word = word.toUpperCase();
-    let wordScore = 0;
-    for (const letter of word) {
-      wordScore += scoreDict[letter];
-    }
-    if (7 <= word.length) {
-      wordScore += 8;
-    }
+  word = word.toUpperCase();
+  let wordScore = 0;
+  for (const letter of word) {
+    wordScore += scoreDict[letter];
+  }
+  if (7 <= word.length) {
+    wordScore += 8;
+  }
   return wordScore;
 };
 
@@ -104,40 +103,41 @@ export const highestScoreFrom = (words) => {
   // use map function to calculate score for each word and add {word, score} to list
   const buildWordScoreObject = (word) => {
     let wordScore = {};
-    wordScore['word'] = word;
-    wordScore['score'] = scoreWord(word);
+    wordScore["word"] = word;
+    wordScore["score"] = scoreWord(word);
     return wordScore;
-  }
+  };
 
   const wordScores = words.map(buildWordScoreObject);
-  
+
   let highScore = 0;
-  
+
   // iterate through wordScores and compare each score to highScore
   let winner;
 
   for (const wordObj of wordScores) {
-
-  // if word.score is greater, let winner = word, update highScore to word.score
-    if (wordObj['score'] > highScore) {
+    // if word.score is greater, let winner = word, update highScore to word.score
+    if (wordObj["score"] > highScore) {
       winner = wordObj;
-      highScore = wordObj['score'];
-  // if a tie, check whether lengths are equal OR if current winner is 10 letters: if yes, prefer current winner
-    } else if (wordObj['score'] === highScore) {
-        if ((wordObj['word'].length === winner['word'].length) || (winner['word'].length === 10)) {
-      //do nothing  
-          
-      // if not, check if length of new word is 10: if yes, it's the winner
-        } else if (wordObj['word'].length === 10) {
-          winner = wordObj;
-          highScore = wordObj['score'];
+      highScore = wordObj["score"];
+      // if a tie, check whether lengths are equal OR if current winner is 10 letters: if yes, prefer current winner
+    } else if (wordObj["score"] === highScore) {
+      if (
+        wordObj["word"].length === winner["word"].length ||
+        winner["word"].length === 10
+      ) {
+        //do nothing
+        // if not, check if length of new word is 10: if yes, it's the winner
+      } else if (wordObj["word"].length === 10) {
+        winner = wordObj;
+        highScore = wordObj["score"];
         // else, prefer the shorter word
-        } else if (wordObj['word'].length < winner['word'].length) {
-          winner = wordObj;
-          highScore = wordObj['score'];
-        }
+      } else if (wordObj["word"].length < winner["word"].length) {
+        winner = wordObj;
+        highScore = wordObj["score"];
       }
     }
+  }
   // once all words have been considered, return the winner
   return winner;
-}
+};
