58056954c6a3d44a94bc01c4f79a3c6e2fedf594
Jessica H <jessicahebert75@gmail.com>
2022-12-13 08:12:12 -0800

    3 out of 4

--- src/adagrams.js
+++ src/adagrams.js
@@ -57,11 +57,70 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   }
   return true;
 };
+const letterScores = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+
+  let score = 0;
+  let bonus = 8;
+  word = word.toUpperCase();
+
+  for (let i = 0; i < word.length; i++) {
+    score += letterScores[word[i]];
+  }
+
+  if (word.length >= 7 && word.length < 11) {
+    score += bonus;
+  }
+  return score;
+    
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+  let highestWord = '';
+  let highestScore = 0;
+
+  for (let word of words){
+    let score = scoreWord(word);
+    if (score> highestScore){
+      highestScore = score;
+      highestWord = word;
+    }else if (score == highestScore){
+      if(highestWord.length==10){
+        break
+      }else if (word.length == 10 || word.length< highestWord.length){
+        highestWord = word;
+        highestScore = score;
+        }
+    }
+  } 
+  return {'score':highestScore, 'word':highestWord}
 };
