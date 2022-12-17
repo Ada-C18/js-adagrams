dff2574a5a5c9bb11cc3c9addcc73ae05a9385a6
Evelyn <hay.evelyn@gmail.com>
2022-12-17 12:49:21 -0800

    finished waves 1 and 2. Passes all tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,90 @@
+//import { hasOwnMetadata } from "core-js/fn/reflect";
+
+//import { forEach } from "core-js/core/array";
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const hand = [];
+
+  const letterPool = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2, 
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+  };
+
+  const makePoolArr = function() {
+    const poolArr = [];
+    for (const [letter, quantity] of Object.entries(letterPool)) {
+      for (let i = 0; i < quantity; i++) {
+        poolArr.push(letter);
+      }
+    }
+
+    return poolArr
+  };
+  
+  const getRandNum = function(poolArr) {
+    const maxLength = poolArr.length - 1;
+    const randNum = Math.floor(Math.random() * maxLength);
+    return randNum
+  };
+
+  const poolArr = makePoolArr();
+
+  while(hand.length < 10) {
+    //let letterPoolCopy = letterPool;
+    //const poolArr = makePoolArr();
+    const randNum = getRandNum(poolArr);
+    const letterValue = function () {
+      return poolArr[randNum];
+    }
+      
+    hand.push(letterValue());
+    poolArr.splice(randNum, 1);
+      //letterPool[letterValue()] = (letterPool[letterValue()]) - 1;
+
+  };
+  
+  return hand
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let validLetterCounter = 0
+  for(let letter of input) {
+      let index = lettersInHand.indexOf(letter);
+      if(lettersInHand.includes(letter)){
+        lettersInHand.splice(index, 1);
+        validLetterCounter += 1
+      } else {
+        return false
+      };
+    };
+  if(input.length === validLetterCounter){
+    return true  
+  };
+
 };
 
 export const scoreWord = (word) => {
