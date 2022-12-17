c395c4c591fdb2d367a93fe23ea1ea2374d34d62
Evelyn <hay.evelyn@gmail.com>
2022-12-17 14:43:32 -0800

    wave 3 done. passes all tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -88,7 +88,56 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const scoreChart = {
+    "A": 1,
+    "B": 3,
+    "C": 3,
+    "D": 2,
+    "E": 1,
+    "F": 4,
+    "G": 2,
+    "H": 4,
+    "I": 1,
+    "J": 8,
+    "K": 5,
+    "L": 1,
+    "M": 3,
+    "N": 1,
+    "O": 1,
+    "P": 3,
+    "Q": 10,
+    "R": 1,
+    "S": 1,
+    "T": 1,
+    "U": 1,
+    "V": 4,
+    "W": 4,
+    "X": 8,
+    "Y": 4,
+    "Z": 10
+  }
+  // for(let [letter, ptValue] in scoreChart){
+  //   console.log(scoreChart[letter])
+  // };
+
+  let score = 0
+  //console.log(score)
+  //console.log('word:', word)
+  if(word === "") {
+    return score
+  };
+
+  for(let letter of word.toUpperCase()) {
+    //console.log(letter)
+    score += scoreChart[letter];
+    //console.log(score)
+  }
+
+  if(word.length > 6){
+    score += 8;
+  }
+  
+  return score
 };
 
 export const highestScoreFrom = (words) => {
