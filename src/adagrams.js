39e0eb0dd3c7354343c255fa91d83fd1c39caa02
Diana <diana38974@gmail.com>
2023-06-06 19:59:56 -0400

    Implementation of usesAvailableLetters and scoreWord functions.

--- src/adagrams.js
+++ src/adagrams.js
@@ -49,19 +49,78 @@ while (hand.length < 10) {
     hand.push(letter);
   }
 }
-
 console.log(hand);
 return hand
-
-
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
+  input = input.toUpperCase();
+  let isWordValid = true;
+
+  for (let i = 0; i < input.length; i++) {
+    const letter = input[i];
+
+    if (!lettersInHand.includes(letter)) {
+      iswordValid = false;
+    } else {
+      const v = lettersInHand.filter(l => l === letter).length;
+      const v2 = word.split(letter).length - 1;
+
+      if (v2 > v) {
+        isWordValid = false;
+      }
+    }
+  }
+  console.log(isWordValid);
+  return isWordValid;
+}
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const scoreChart = {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4,
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10
+  };
+
+  word = word.toUpperCase();
+  let scoreSum = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    const value = scoreChart[letter];
+    scoreSum += value;
+  }
+  console.log(word.length);
+
+  if (word.length >= 7 && word.length <= 10) {
+    scoreSum += 8;
+  }
+  console.log(scoreSum);
+  return scoreSum;
 };
 
 export const highestScoreFrom = (words) => {
