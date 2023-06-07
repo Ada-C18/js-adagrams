3fd0c2525a51b5b1c5cdc71fa46ab3d6cce840ba
Pha'lesa Patton <ppatton86@gmail.com>
2023-06-07 15:17:09 -0400

    playing with syntax

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,61 @@
+const LETTERPOOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let letterList = []
+  let tileList = []
+  
+  for (let [key, value] of Object.entries(LETTERPOOL)){
+    letterList.push(key.repeat(value));
+  }
+    
+  for (let ele of letterList) {
+      for (let letter of ele) {
+        tileList.push(letter);
+    }
+  return tileList
+  }
+
+  let drawnTiles = [];
+  for (let letters of tileList) {
+    while (drawnTiles.length < 10) {
+      let indexNum = Math.floor(Math.random() * (tileList.Length +1));
+      let tile = tileList[indexNum];
+      drawnTiles.push(tile);
+    }
+  return drawnTiles
+  }
+  
 };
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
