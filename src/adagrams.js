ccd67f3af00da3429ed3165202db4774ebcc2166
Pha'lesa Patton <ppatton86@gmail.com>
2023-06-08 16:28:11 -0400

    Score Calculator functioning

--- src/adagrams.js
+++ src/adagrams.js
@@ -104,8 +104,54 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const SCORECHART = {
+    'A':1,
+    'E':1,
+    'I':1,
+    'O':1,
+    'U':1,
+    'L':1,
+    'N':1,
+    'R':1,
+    'S':1,
+    'T':1,
+    'D':2,
+    'G':2,
+    'B':3,
+    'C':3,
+    'M':3,
+    'P':3,
+    'F':4,
+    'H':4,
+    'V':4,
+    'W':4,
+    'Y':4,
+    'K':5,
+    'J':8,
+    'X':8,
+    'Q':10,
+    'Z':10
+  };
+
+  let sumScore = 0;
+  const words = word.toUpperCase();
+
+  
+  
+  for (const letter of words) {
+    let value = SCORECHART[letter];
+    sumScore += value;
+  }
+  if (words.length >= 7 && words.length <= 10) {
+    sumScore += 8;
+  }
+
+
+return sumScore;
+
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
+  
 };
