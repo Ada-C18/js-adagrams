2eb16c5a7d089d4eb15138c060a287e94e7b2391
Pha'lesa Patton <ppatton86@gmail.com>
2023-06-07 22:44:10 -0400

    Wave 2 If all test

--- src/adagrams.js
+++ src/adagrams.js
@@ -28,37 +28,77 @@ const LETTERPOOL = {
 };
 
 
+const createLetterList = () => {
+  const letters = [];
+  const letterList = [];
+  for (const [key, value] of Object.entries(LETTERPOOL)) {
+    letters.push(key.repeat(value)) ;
+  }
+  for (const ele of letters) {
+    for (const letter of ele) {
+      letterList.push(letter);
+    }
+  }
+  console.log(letterList)
+  return letterList;
+};
+
+
+
+const pool = createLetterList();
+
+const createRandomizer = () => {
+  const randomIndex = Math.floor(Math.random() * (pool.length + 1));
+  const randomLetter = pool[randomIndex];
+  return randomLetter
+};
+
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
-  let letterList = []
-  let tileList = []
-  
-  for (let [key, value] of Object.entries(LETTERPOOL)){
-    letterList.push(key.repeat(value));
-  }
-    
-  for (let ele of letterList) {
-      for (let letter of ele) {
-        tileList.push(letter);
-    }
-  return tileList
-  }
+  // const pool = createLetterList(LETTERPOOL);
+  const drawnLetters = [];
 
-  let drawnTiles = [];
-  for (let letters of tileList) {
-    while (drawnTiles.length < 10) {
-      let indexNum = Math.floor(Math.random() * (tileList.Length +1));
-      let tile = tileList[indexNum];
-      drawnTiles.push(tile);
+  while (drawnLetters.length < 10) {
+    const randomLetter = createRandomizer();
+    const occurences = drawnLetters.filter( letter => letter === randomLetter).length
+
+    if (occurences < LETTERPOOL[randomLetter]) {
+      drawnLetters.push(randomLetter);
     }
-  return drawnTiles
   }
+  return drawnLetters;
   
 };
 
 
+
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  const word = input.toUpperCase();
+  const wordArray = word.split('');
+  
+  
+  for (const letter of word) {
+    const occurencesHand = lettersInHand.filter( letter => letter === letter).length; 
+    const occurencesWord = wordArray.filter( letter => letter === letter).length;
+    if (occurencesWord <= occurencesHand) {
+      if (lettersInHand.includes(letter)) {
+        return true
+
+    }
+      else {
+        return false
+      }
+      
+    }
+    
+    else {
+      return false
+    }
+  }
+  
 };
 
 export const scoreWord = (word) => {
