8d06f3fbefeee5aa46307dddacfbcd0b47c8c200
an2084 <angelanawang47@gmail.com>
2023-06-12 07:49:09 -0400

    Wave 3 completed

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,4 +1,4 @@
-let LETTER_POOL = {
+const LETTER_POOL = {
   'A': 9, 
   'B': 2, 
   'C': 2, 
@@ -32,6 +32,7 @@ export const drawLetters = () => {
   for (const [key, value] of Object.entries(LETTER_POOL)) {
     let letter = Array(value).fill(key);
     letterPoolArr.push(...letter);
+  }
 
   let letterBank = [];
   while (letterBank.length < 10) {
@@ -58,9 +59,31 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  const SCORE_CHART = {
+    "A": 1, "B": 3, "C": 3, "D": 2,
+    "E": 1, "F": 4, "G": 2, "H": 4,
+    "I": 1, "J": 8, "K": 5, "L": 1,
+    "M": 3, "N": 1, "O": 1, "P": 3,
+    "Q": 10, "R": 1, "S": 1, "T": 1,
+    "U": 1, "V": 4, "W": 4, "X": 8,
+    "Y": 4, "Z": 10
+  };
+
+  let sum = 0;
+  word = word.toUpperCase();
+
+  const higherPointValues = [7, 8, 9, 10];
+  for (let letter of word) {
+    let pointValue = SCORE_CHART[letter];
+    sum += pointValue;
+  }
+
+  if (higherPointValues.includes(word.length)) {
+    sum += 8;
+  }
+  return sum;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+
 };
